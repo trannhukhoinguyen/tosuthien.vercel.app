527be325e871ab0f52038ba0759f6ac6b8743f28
Miguel <miguel@glodival.vn>
2024-09-05 10:18:51 +0700

    update

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,5 @@
 import { defineCollection, z } from "astro:content";
 
-const blogsCollection = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    meta_title: z.string().optional(),
-    description: z.string().optional(),
-    date: z.date().optional(),
-    image: z.string().optional(),
-    master: z.string().default(""),
-    categories: z.array(z.string()).default([""]),
-    tags: z.array(z.string()).default([""]),
-    draft: z.boolean().optional(),
-  }),
-});
 const faqsCollection = defineCollection({
   schema: z.object({
     title: z.string(),
@@ -72,15 +59,6 @@ const pagesCollection = defineCollection({
     image: z.string().optional(),
     draft: z.boolean().optional(),
   }),
-});
-
-// Schema
-const dharmaRealm = z.object({
-  title: z.string(),
-  meta_title: z.string().optional(),
-  description: z.string().optional(),
-  image: z.string(),
-  draft: z.boolean().optional(),
 });
 
 // Banner schema
@@ -145,13 +123,11 @@ const callToActionSchema = z.object({
 
 // Export collections
 export const collections = {
-  blogs: blogsCollection,
   faqs: faqsCollection,
   koans: koansCollection,
   cautions: cautionsCollection,
   masters: mastersCollection,
   pages: pagesCollection,
-  dharmaRealm: dharmaRealm,
   content: contentCollection,
   huatous: huatousSchema,
   callToAction: callToActionSchema,
