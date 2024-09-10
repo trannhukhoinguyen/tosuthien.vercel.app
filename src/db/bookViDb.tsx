18c7ce34f573edda349434d0f09b50bb42dc3fa0
Miguel <miguel@glodival.vn>
2024-09-10 16:23:59 +0700

    update bookcase, download, bookViDb

---
+++ src/db/bookViDb.tsx
@@ -0,0 +1,25 @@
+const bookViDb = [
+  {
+    id: 1,
+    cover: 'https://i.ytimg.com/vi/RFhGmMxOP6I/maxresdefault.jpg',
+    title: 'BÁ TRƯỢNG NGỮ LỤC',
+    author: 'Thiền Sư Bá Trượng Hoài Hải',
+    description: 'Việt Dịch: HT. Thích Duy Lực',
+    info: '123',
+    link: 'http://tosuthien.info/kinh_sach_storage/ba%20truong%20ngu%20luc.pdf',
+    youtube: 'RFhGmMxOP6I',
+    mp3: '/ftp/MP3/- T Duy Luc/Góp Nhặt Lời Phật Tổ/11 Bá Trượng Ngữ Lục.mp3',
+  },
+  {
+    id: 2,
+    cover: '/images/avatar.png',
+    title: '222222222',
+    author: 'author2222',
+    description: 'description2222',
+    info: 'info2222',
+    link: '/images/avatar.png',
+    youtube: 'RFhGmMxOP6I',
+    mp3: 'RFhGmMxOP6I',
+  },
+]
+export default bookViDb;
