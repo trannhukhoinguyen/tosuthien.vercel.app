25e7496f945fd3d68e5912cba35b719b10c5df02
Miguel <miguel@glodival.vn>
2024-09-16 14:59:17 +0700

    update monasteries

---
+++ src/db/maps/mapDb.tsx
@@ -0,0 +1,25 @@
+const mapDb = [
+  {
+    id: 1,
+    key: 'gao-min',
+    cover: 'https://peacelilysite.com/wp-content/uploads/2022/09/6782f74c-b11f-4bbb-bf50-973e4e6680d3.jpeg',
+    titleEn: 'GAO MIN MONASTERY',
+    titleVi: 'THIỀN VIỆN CAO MÂN ',
+    mapCenter: [32.32652, 119.41237],
+    markerPosition: [32.32652, 119.41237],
+    zoomLevel: 13,
+    address: '8CF8+2V7, Gaominsi Rd, Hanjiang District, Yangzhou, Yangzhou, Jiangsu, China, 225127',
+  },
+  {
+    id: 2,
+    key: 'tu-an',
+    cover: 'https://tosuthien.com/wp-content/uploads/2021/11/245606675_242571897750276_3066781601464414675_n.jpg',
+    titleEn: 'TU AN ZEN TEMPLE',
+    titleVi: 'TỪ ÂN THIỀN ĐƯỜNG ',
+    mapCenter: [33.74861, -117.92682],
+    markerPosition: [33.74861, -117.92682],
+    zoomLevel: 15,
+    address: '4310 W 5th St, Santa Ana, California, California, US',
+  },
+]
+export default mapDb;
