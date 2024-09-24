3eb2992ff616656839475e2bd65aed6a0b461dd4
Miguel <miguel@glodival.vn>
2024-09-24 10:57:30 +0700

    update css responsive, master db

--- src/layouts/shortcodes/Table.tsx
+++ src/layouts/shortcodes/Table.tsx
@@ -72,7 +72,7 @@ const Table: React.FC<TableProps> = ({ data, lang, itemsPerPage = 10 }) => {
   const lastPage = filteredData.length % itemsPerPage === 0 ? filteredData.length / itemsPerPage : Math.ceil(filteredData.length / itemsPerPage);
 
   return (
-    <div className="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 pr-10 lg:px-8">
+    <div className="-my-2 py-2 overflow-x-auto sm:-mx-6 lg:-mx-8 lg:px-8 w-full">
 
       <div className="align-middle rounded-tl-lg rounded-tr-lg inline-block w-full py-4 overflow-hidden bg-white shadow-lg px-12">
         <div className="flex justify-between">
