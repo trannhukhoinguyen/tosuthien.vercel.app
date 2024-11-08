30f8612efd663d3c5a587d8ecf2fa2e7004452bf
Miguel <miguel@glodival.vn>
2024-11-09 00:33:20 +0700

    update

--- src/db/teachings/GuishanAlert.tsx
+++ src/db/teachings/GuishanAlert.tsx
@@ -1,18 +1,21 @@
 export const title = "Quy Sơn Cảnh Sách";
-export const subTitle = "subTitle";
+export const subTitle = "Việt Dịch: Nguyễn Minh Tiến";
 export const author = "[Thiền sư] Quy Sơn Linh Hựu";
-export const image = "/images/teachings/guishan-alert.jpg";
-export const description = "description";
+export const image = "/images/teachings/guishan-alert.jpg" || "/images/teachings/guishan-alert-1.jpg";
+export const intro = "Quyển Qui Sơn Cảnh Sách là một quyển luận của Thiền sư Linh Hựu. Do Ngài\n" +
+  "ở tại núi Qui, người đương thời nể trọng đức hạnh không dám gọi tên, nên dùng\n" +
+  "tên núi để gọi Ngài ; vì vậy Ngài có tên là Qui Sơn. Quyển luận này nhằm mục\n" +
+  "đích nhắc nhở người xuất gia phải nổ lực tu hành, để được giải thoát.";
 
-export const content =
-  "- Do nghiệp lực trói buộc mà có thân này, không khỏi bao điều khổ lụy. Thân thể từ cha mẹ sanh ra, vốn thật chỉ là giả hợp các duyên." +
+export const content1 =
+  "Do nghiệp lực trói buộc mà có thân này, không khỏi bao điều khổ lụy. Thân thể từ cha mẹ sanh ra, vốn thật chỉ là giả hợp các duyên." +
   "Dù là nương nơi bốn đại mà tồn tại, nhưng bốn đại ấy chẳng lúc nào là không xung khắc." +
   "Chuyện vô thường già chết không hề hẹn trước." +
   "Sớm còn tối mất, thoắt chốc  đã sang đời khác; như sương như móc, thoạt có thoạt không; như cây ven bờ vực, như dây leo vách giếng, có chi bền chắc?" +
   "Niệm niệm nhanh chóng nối nhau, chỉ trong chớp mắt, trút hơi thở đã qua kiếp khác." +
-  "Sao lại có thể yên lòng mà bỏ phí cuộc đời trôi qua vô ích?" +
-  " / " +
-  "- Làm người xuất gia, chẳng thể dâng cho cha mẹ miếng ăn ngon ngọt, cho đến quyến thuộc cũng đều dứt bỏ." +
+  "Sao lại có thể yên lòng mà bỏ phí cuộc đời trôi qua vô ích?"
+export const content2 =
+  "Làm người xuất gia, chẳng thể dâng cho cha mẹ miếng ăn ngon ngọt, cho đến quyến thuộc cũng đều dứt bỏ." +
   "Không lo việc nước, cũng chẳng kế tục nghiệp nhà; xa lìa xóm giềng thân tộc, xuống tóc theo thầy học đạo." +
   "Vậy nên trong lòng phải biết chuyên cần sớm tối công phu, ngoài học lấy đức hiền hòa không tranh chấp; xa lánh chốn thế tục mà một lòng cầu giải thoát." +
   "Lẽ nào vừa được thọ giới phẩm liền tự xưng là bậc tỳ-kheo, ăn hạt cơm của tín thí thập phương chẳng chịu xét cho cùng do đâu mà có, chỉ nói bừa rằng việc ấy là lẽ tự nhiên." +
@@ -23,11 +26,11 @@ export const content =
   "Đấng đạo sư đã có lời khuyên răn khuyến khích, tỳ-kheo phải tiến lên trên đường đạo, chớ nên buông thả thân mình; chuyện ăn mặc ngủ nghỉ đừng bao giờ hưởng thụ cho đầy đủ." +
   "Người ta vốn phần đông tham đắm vào những chuyện ấy, chỉ quanh quẩn mãi thoắt chốc đã hết đời." +
   "Vậy nên kẻ hậu học chưa hiểu sâu tông chỉ cần phải siêng năng cầu học với người đã biết." +
-  "Sao lại toan nói rằng việc xuất gia quý cầu no cơm ấm áo?" +
-  " / " +
-  "- Phật trước kia chế định giới luật, mở bày chỉ dạy người tăm tối; oai nghi phép tắc trong sạch như băng tuyết." +
+  "Sao lại toan nói rằng việc xuất gia quý cầu no cơm ấm áo?"
+export const content3 =
+  "Phật trước kia chế định giới luật, mở bày chỉ dạy người tăm tối; oai nghi phép tắc trong sạch như băng tuyết." +
   "Kẻ mới phát tâm nương theo đó mà ngăn chặn mọi điều sai phạm." +
-  "Phép tắc tinh vi, dẹp bỏ hết những điều tồi tệ." +
+  "Phép tắc tinh vi, dẹp bỏ hết những điều tồi tệ. " +
   "Nơi truyền dạy giới luật chưa từng lạm được nương theo,2 pháp rốt ráo thượng thừa làm sao hiểu thấu?" +
   "Thật đáng tiếc thay, bỏ phí một đời trôi qua vô ích, sau dù hối tiếc cũng không còn kịp nữa." +
   "Lời Phật dạy không chịu ghi lòng tạc dạ thì đạo nhiệm mầu không thể do đâu mà tỏ ngộ." +
@@ -37,9 +40,9 @@ export const content =
   "To tiếng nặng lời khoác lác huyênh hoang, chẳng biết kính nhường kẻ trên người dưới, so ra khác gì ngoại đạo?" +
   "Đến bữa ăn khua bát ồn ào, ăn vội vàng đứng dậy đi trước." +
   "Đi ở không tuân phép tắc, chẳng ra dáng vẻ bậc xuất gia; đứng ngồi hoảng loạn, náo động tâm người khác." +
-  "Phép tắc ít nhiều chẳng giữ, oai nghi lớn nhỏ đều không, chẳng dựa vào đâu để răn đe kẻ dưới, mà người mới nhập đạo nhìn vào cũng chẳng thấy có gì để noi theo học." +
-  " / " +
-  "- Như có ai vừa chỉ ra chỗ sai sót, liền bảo rằng: Như ta đây mới thật bậc ẩn tu!" +
+  "Phép tắc ít nhiều chẳng giữ, oai nghi lớn nhỏ đều không, chẳng dựa vào đâu để răn đe kẻ dưới, mà người mới nhập đạo nhìn vào cũng chẳng thấy có gì để noi theo học."
+export const content4 =
+  "Như có ai vừa chỉ ra chỗ sai sót, liền bảo rằng: Như ta đây mới thật bậc ẩn tu!" +
   "Chưa nghe làm theo lời Phật dạy, chỉ một lòng ôm giữ tánh tình thô thiển." +
   "Chỗ thấy biết như vậy, chung quy cũng là vì khi mới nhập đạo biếng nhác chẳng tinh cần, tham đắm thế tục, lần lữa qua ngày." +
   "Thấm thoát hết đời thành ra buông lung thô lỗ." +
@@ -49,132 +52,131 @@ export const content =
   "Bấy giờ mới sớm tối lo sợ, tâm tưởng hoang mang. Đường sắp tới u ám mê mờ chẳng biết về đâu!" +
   "Trong lòng hối tiếc cũng không còn kịp nữa, như kẻ khát nước mới đi đào giếng, có ích lợi gì?" +
   "Tự hận mình trước đã không sớm lo tu tập, để khi tuổi già chất chứa đầy tội lỗi." +
-  "Cái chết gần kề trong thoáng chốc, hãi hùng khiếp sợ biết bao!" +
-  " / " +
-  "- Trút hơi thoát khỏi cuộc đời, như con chim bay xuyên qua dải lụa, nghiệp lực dắt dẫn thần thức trôi lăn." +
+  "Cái chết gần kề trong thoáng chốc, hãi hùng khiếp sợ biết bao!"
+export const content5 =
+  "Trút hơi thoát khỏi cuộc đời, như con chim bay xuyên qua dải lụa, nghiệp lực dắt dẫn thần thức trôi lăn." +
   "Như người nhiều nợ lắm chủ theo đòi, ai mạnh được trước; tạo nghiệp đã nhiều, phải tùy theo chỗ nặng nề nhất mà thọ thân gánh chịu." +
   "Con quỷ vô thường luôn chực cướp đi sinh mạng của mình, chẳng lúc nào ngưng." +
   "Đời người hạn cuộc chẳng thể kéo dài, thời gian trôi qua không hề chờ đợi." +
   "Ba cõi luân hồi chưa thoát được ra thì cứ phải như thế mà mãi mãi thọ thân lưu chuyển." +
   "Lạ lùng thương cảm biết bao! Trong lòng thương xót thống thiết, há có thể ngậm miệng không nói?" +
-  "Nên muốn nhắc nhở cảnh tỉnh cho nhau." +
-  " / " +
-  "- Đáng buồn là chúng ta sanh ra cuối thời tượng pháp, cách xa các bậc thánh hiền, pháp  Phật  chẳng mấy ai am hiểu, nhiều người giải đãi." +
+  "Nên muốn nhắc nhở cảnh tỉnh cho nhau." ;
+export const content6 =
+  "Đáng buồn là chúng ta sanh ra cuối thời tượng pháp, cách xa các bậc thánh hiền, pháp  Phật  chẳng mấy ai am hiểu, nhiều người giải đãi." +
   "Vì vậy lược nói ra đây đôi chút thấy biết hẹp hòi để khuyên răn những người hậu học." +
   "Nếu chẳng từ bỏ sự kiêu căng cao ngạo, nết cũ thật khó chuyển đổi!" +
   "Người đã xuất gia, cất bước vượt lên cao xa; tâm tánh, cốt cách khác người thế tục." +
   "Tiếp nối mà làm hưng thịnh đạo pháp, nhiếp phục hết thảy những thói hư tật xấu." +
   "Lấy việc ấy mà báo đáp bốn ơn, bạt khổ cứu nguy khắp trong ba cõi." +
-  "Nếu không được vậy, chỉ là kẻ lạm mang hình tướng xuất gia, lời nói việc làm phóng túng lơ đễnh, uổng nhận sự cúng dường của thập phương tín thí." +
-  " / " +
-  "- Như người giậm chân tại chỗ, dù nhiều năm qua một tấc chẳng dời!" +
+  "Nếu không được vậy, chỉ là kẻ lạm mang hình tướng xuất gia, lời nói việc làm phóng túng lơ đễnh, uổng nhận sự cúng dường của thập phương tín thí.";
+export const content7 =
+  "Như người giậm chân tại chỗ, dù nhiều năm qua một tấc chẳng dời!" +
   "Mơ màng thoắt chốc qua hết một đời, chẳng biết nương vào đâu làm chỗ dựa!" +
   "Huống chi, đường đường mang hình tướng của một vị tăng, dáng vẻ tốt đẹp, đều là quả báo có được nhờ căn lành gieo trồng từ thuở trước." +
   "Lẽ đâu lại khoanh tay ngồi yên để thời gian trôi qua không hối tiếc? Sự nghiệp nếu chẳng chuyên cần, dựa vào đâu mà mong thành đạo quả?" +
   "Chẳng những một đời vô ích, cho đến kiếp sau cũng chẳng được gì!" +
-  "Quyết tâm rời bỏ mẹ cha, khoác áo nâu sòng là ý muốn vượt trên thế tục." +
-  " / " +
-  "- Sớm tối khắc ghi việc ấy, lẽ đâu để luống ngày qua? Nguyện làm được bậc trụ cột trong Phật pháp, nêu gương hậu thế." +
+  "Quyết tâm rời bỏ mẹ cha, khoác áo nâu sòng là ý muốn vượt trên thế tục.";
+export const content8 =
+  "Sớm tối khắc ghi việc ấy, lẽ đâu để luống ngày qua? Nguyện làm được bậc trụ cột trong Phật pháp, nêu gương hậu thế." +
   "Thường mong muốn như thế nhưng chưa được ít nhiều như nguyện." +
   "Mở miệng nói ra phải hợp kinh điển. Luận bàn phải dựa theo những gương sáng thuở xưa." +
   "Hình tướng oai nghi đĩnh đạc, tâm ý khí lực cao cả thanh thoát." +
   "Đi xa phải có bạn hiền, thường giữ cho tai mắt được trong sạch." +
-  "Trú ngụ phải chọn cùng người tốt, thường nghe những việc chưa nghe." +
-  " / " +
-  "- Cho nên nói: “Sinh ta ra là cha mẹ, giúp ta thành người là bạn hữu”." +
+  "Trú ngụ phải chọn cùng người tốt, thường nghe những việc chưa nghe.";
+export const content9 =
+  "Cho nên nói: “Sinh ta ra là cha mẹ, giúp ta thành người là bạn hữu”." +
   "Được gần người hiền như đi giữa đám hơi sương, tuy không ướt áo nhưng lúc nào cũng được ẩm mát." +
   "Gần kẻ ác thì nuôi lớn thêm điều ác trong chỗ thấy biết; sớm tối làm việc ác liền phải chịu lấy quả báo trước mắt." +
   "Sau khi chết rồi phải chịu chìm đắm; thân người một khi mất đi, muôn kiếp khó lòng được lại." +
   "Lời nói thẳng trái tai, sao không khắc ghi vào tâm khảm?" +
-  "Theo đó liền có thể lắng sạch tâm trí, nuôi dưỡng đức hạnh, lui về chỗ vắng vẻ không còn hình tích danh tánh, giữ lòng chuyên chú trong sạch mà dứt hết sự ồn ào nhiễu loạn." +
-  " / " +
-  "- Như muốn tham thiền học đạo, vượt thẳng qua những pháp môn dùng làm phương tiện, tâm phải hợp được với tôn chỉ huyền diệu, " +
+  "Theo đó liền có thể lắng sạch tâm trí, nuôi dưỡng đức hạnh, lui về chỗ vắng vẻ không còn hình tích danh tánh, giữ lòng chuyên chú trong sạch mà dứt hết sự ồn ào nhiễu loạn.";
+export const content10 =
+  "Như muốn tham thiền học đạo, vượt thẳng qua những pháp môn dùng làm phương tiện, tâm phải hợp được với tôn chỉ huyền diệu, " +
   "cứu xét chi ly chỗ tinh yếu, quyết định chọn lấy chỗ sâu xa, hiểu thấu cội nguồn chân thật. Rộng đường học hỏi với những người đi trước, gần gũi bạn hiền." +
   "Cách tu này rất khó đạt đến chỗ kỳ diệu, nên phải khẩn thiết dụng tâm tinh tế, mới có thể do trong ấy mà tức thời hiểu được chỗ cốt yếu nhất, " +
   "liền nương theo đó mà dần dần ra khỏi trần tục." +
   "Như vậy tức là phá sạch hết hai mươi lăm cảnh có trong ba cõi." +
   "Hết thảy các pháp trong ngoài đều rõ biết là không thật." +
-  "Do tâm khởi hiện, tất cả đều là tên gọi không thật." +
-  " / " +
-  "- Chẳng cần dùng tâm để hiểu thấu, chỉ cần tâm không đắm nhiễm nơi vật, thì vật cũng không ngăn ngại được người." +
+  "Do tâm khởi hiện, tất cả đều là tên gọi không thật.";
+export const content11 =
+  "Chẳng cần dùng tâm để hiểu thấu, chỉ cần tâm không đắm nhiễm nơi vật, thì vật cũng không ngăn ngại được người." +
   "Mặc tình cho muôn pháp chuyển xoay, không dứt bỏ cũng không nối tiếp." +
   "Nghe biết âm thanh hình sắc, thảy đều  là những chuyện bình thường; dù bên này hay bên kia, chỗ ứng dụng thảy đều đầy đủ." +
   "Chỗ làm nếu được như thế, thật không uổng đã mặc áo xuất gia. Báo đáp được bốn ơn, bạt khổ cứu nguy khắp trong ba cõi." +
   "Như trong nhiều kiếp vẫn có thể không thối chí, thì quả Phật nhất định đạt tới." +
   "Đối với ba cõi không còn ràng buộc, chỉ như người khách đến đi, lúc hiện lúc ẩn đều có thể làm khuôn phép cho kẻ khác." +
   "Tu theo phép học đạo tham thiền như vậy, quả là pháp môn huyền diệu hơn hết." +
-  "Chỉ cần có đủ quyết tâm, pháp không sai dối." +
-  " / " +
-  "- Như với người ở mức bình thường, không thể nhất thời vượt thoát, thì nên hết sức chú tâm tìm tòi học hỏi giáo pháp." +
+  "Chỉ cần có đủ quyết tâm, pháp không sai dối.";
+export const content12 =
+  "Như với người ở mức bình thường, không thể nhất thời vượt thoát, thì nên hết sức chú tâm tìm tòi học hỏi giáo pháp." +
   "Thông thuộc kinh điển, nghiền ngẫm cứu xét nghĩa lý tinh tường, rồi truyền rộng ra khắp nơi, dẫn dắt người hậu học, báo đáp ơn đức Phật." +
   "Thời gian trôi qua, chớ nên luống mất, phải nên lấy sự tu tập như trên mà làm chỗ dựa đời mình." +
-  "Giữ lấy oai nghi, thành bậc pháp khí giữa chúng tăng." +
-  " / " +
-  "-  Chẳng thấy như dây leo kia, nhờ dựa vào thân cây tùng mà lên được đến tầng cao chót vót." +
+  "Giữ lấy oai nghi, thành bậc pháp khí giữa chúng tăng.";
+export const content13 =
+  "Chẳng thấy như dây leo kia, nhờ dựa vào thân cây tùng mà lên được đến tầng cao chót vót." +
   "Hãy chọn nhân lành cao trổi mà ký thác đời mình, mới có thể rộng làm lợi ích." +
   "Phải hết lòng tu tập, giữ gìn trai giới, đừng khinh thường mà giảm bớt hoặc bỏ qua." +
-  "Đời đời kiếp kiếp về sau nhờ đó mà được hưởng quả phước rất nhiệm mầu." +
-  " / " +
-  "-  Không nên nhàn nhã để phí ngày qua, biếng nhác bỏ đi thời khắc." +
+  "Đời đời kiếp kiếp về sau nhờ đó mà được hưởng quả phước rất nhiệm mầu.";
+export const content14 =
+  "Không nên nhàn nhã để phí ngày qua, biếng nhác bỏ đi thời khắc." +
   "Thời gian quý giá biết bao, sao chẳng cầu vươn tới?" +
   "Uổng nhận của thập phương tín thí, lại phụ cả bốn ơn." +
-  "Phiền lụy chất chứa càng nhiều, bụi trần khuất lấp tâm trí, đường tới thành ra ngăn lấp, người người đều khinh chê." +
-  " / " +
-  "- Người xưa nói: “Đã là trượng phu, ai ai cũng có thể làm nên việc.”" +
+  "Phiền lụy chất chứa càng nhiều, bụi trần khuất lấp tâm trí, đường tới thành ra ngăn lấp, người người đều khinh chê.";
+export const content15 =
+  "Người xưa nói: “Đã là trượng phu, ai ai cũng có thể làm nên việc.”" +
   "Không nên tự hạ thấp mình mà nhụt tâm, thối chí." +
   "Nếu không được như vậy, thật uổng công xuất gia, thoắt chốc qua hết một đời không chút ích lợi." +
   "Thành khẩn cầu mong các  vị  đều  bừng  cao chí khí quyết liệt, mở rộng hoài bão khác người." +
   "Mỗi mỗi việc làm đều noi gương những bậc cao thượng, chẳng buông thả theo kẻ thấp hèn." +
   "Chỉ một đời này quyết tu hành chứng đạo, tự mình lo liệu chẳng dựa vào ai." +
   "Ngăn dứt vọng niệm, cắt đứt muôn duyên, không còn chạy theo trần cảnh." +
-  "Tâm không cảnh lặng, sự ngăn ngại xưa nay chỉ do chẳng ngộ." +
-  " / " +
-  "- Hãy đọc cho thật kỹ văn này, luôn luôn tỉnh giác gắng tu." +
+  "Tâm không cảnh lặng, sự ngăn ngại xưa nay chỉ do chẳng ngộ.";
+export const content16 =
+  "Hãy đọc cho thật kỹ văn này, luôn luôn tỉnh giác gắng tu." +
   "Mạnh mẽ tự chủ lấy mình, đừng buông thả theo thói thường." +
   "Nghiệp quả dẫn dắt người đi thật khó lòng trốn tránh, như âm thanh thế nào tiếng vọng thế ấy, hình thể có ngay thì bóng soi mới thẳng." +
-  "Nhân quả rõ ràng như thế, lẽ nào không lo sợ?" +
-  " / " +
-  "-  Vì vậy trong kinh nói rằng:  “Dù việc làm đã trải qua trăm ngàn kiếp, nghiệp quả không thể mất." +
+  "Nhân quả rõ ràng như thế, lẽ nào không lo sợ?";
+export const content17 =
+  "Vì vậy trong kinh nói rằng:  “Dù việc làm đã trải qua trăm ngàn kiếp, nghiệp quả không thể mất." +
   "Khi nhân duyên đã hội đủ rồi, quả báo phải tự nhận lấy.”" +
   "Vậy nên biết rằng hình phạt trong ba cõi luôn bám theo mà giết hại người, phải nỗ lực chuyên cần tu tập, đừng bỏ phí ngày qua." +
   "Bởi hiểu thấu được sự nguy hại của tội lỗi, nên mới khuyên nhau tu tập hành trì." +
   "Nguyện sao trong trăm ngàn kiếp, dù sinh ra ở đâu cũng được cùng nhau làm bạn đồng tu."
 ;
-export const extraText = "Có bài TỤNG để khuyên răn rằng";
 export const verse =
-"Thân hư huyễn trong mộng, / " +
-"Hình sắc giữa chốn không. /  " +
-"Việc đã qua không cùng, / " +
-"Việc về sau ai biết? / " +
-"Sanh đây, thác về kia, / " +
-"Chìm nổi, lăn lóc khổ. / " +
-"Chưa thoát ngoài ba cõi, / " +
-"Sao có thể nghỉ yên? / " +
-"Tham luyến chốn thế gian, / " +
-"Năm uẩn duyên hợp thành. / " +
-"Sanh ra mãi đến già, / " +
-"Rốt không được gì cả! / " +
-"Gốc rễ tự vô minh, / " +
-"Từ đó phải mê lầm. / " +
-"Ngày tháng luống trôi qua, / " +
-"Thời khắc không lường được. / " +
-"Một đời này uổng phí, / " +
-"Qua đời khác chẳng thông. / " +
-"Tiếp nối mãi mê lầm, / " +
-"Đều do sáu tên giặc. / " +
-"Lăn lóc giữa sáu đường, / " +
-"Loanh quanh trong ba cõi. / " +
-"Sớm cầu bậc minh sư, / " +
-"Gần gũi người đức hạnh. / " +
-"Thân tâm khéo chọn lựa, / " +
-"Quét hết bao gai góc. / " +
-"Đời nổi trôi phù phiếm, / " +
-"Sao để duyên thúc bức? / " +
-"Phải xét cùng pháp lý, / " +
-"Quyết chứng ngộ đạo mầu. / " +
-"Tâm cảnh đều mất sạch, / " +
-"Không nhớ, không tưởng đến. / " +
-"Sáu căn tùy tự nhiên, / " +
-"Đi ở đều vắng lặng. / " +
-"Tâm định không sanh khởi, / " +
-"Muôn pháp tự dứt lìa. / ";
+"Thân hư huyễn trong mộng / " +
+"Hình sắc giữa chốn không /  " +
+"Việc đã qua không cùng / " +
+"Việc về sau ai biết / " +
+"Sanh đây thác về kia / " +
+"Chìm nổi lăn lóc khổ / " +
+"Chưa thoát ngoài ba cõi / " +
+"Sao có thể nghỉ yên / " +
+"Tham luyến chốn thế gian / " +
+"Năm uẩn duyên hợp thành / " +
+"Sanh ra mãi đến già / " +
+"Rốt không được gì cả / " +
+"Gốc rễ tự vô minh / " +
+"Từ đó phải mê lầm / " +
+"Ngày tháng luống trôi qua / " +
+"Thời khắc không lường được / " +
+"Một đời này uổng phí / " +
+"Qua đời khác chẳng thông / " +
+"Tiếp nối mãi mê lầm / " +
+"Đều do sáu tên giặc / " +
+"Lăn lóc giữa sáu đường / " +
+"Loanh quanh trong ba cõi / " +
+"Sớm cầu bậc minh sư / " +
+"Gần gũi người đức hạnh / " +
+"Thân tâm khéo chọn lựa / " +
+"Quét hết bao gai góc / " +
+"Đời nổi trôi phù phiếm / " +
+"Sao để duyên thúc bức / " +
+"Phải xét cùng pháp lý / " +
+"Quyết chứng ngộ đạo mầu / " +
+"Tâm cảnh đều mất sạch / " +
+"Không nhớ không tưởng đến / " +
+"Sáu căn tùy tự nhiên / " +
+"Đi ở đều vắng lặng / " +
+"Tâm định không sanh khởi / " +
+"Muôn pháp tự dứt lìa ";
