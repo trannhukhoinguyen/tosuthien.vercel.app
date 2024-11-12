2b533990ff1d33607917dd946ede8408401a99be
Miguel <miguel@glodival.vn>
2024-11-12 16:51:02 +0700

    update

---
+++ src/db/teachings/ZenPracticeAlerts.tsx
@@ -0,0 +1,15 @@
+const ZenPracticeAlerts = {
+  title: 'Tham Thiền Cảnh Ngữ',
+  subTitle: 'Việt Dịch: [Thiền sư] Thích Duy Lực',
+  author: '[Thiền sư] Bác Sơn Nguyên Lai',
+  image: '/images/teachings/zhongfeng-speeches.jpg' || '/images/teachings/zhongfeng-dharmar-speeches.jpg',
+  intro:
+    '',
+  paragraph1:
+    '',
+  paragraph2:
+    '',
+  verse:
+    '',
+  }
+export default ZenPracticeAlerts
---
+++ src/db/teachings/ZhongfengSpeeches.tsx
@@ -0,0 +1,15 @@
+const ZhongfengSpeeches = {
+  title: 'Trung Phong Pháp Ngữ',
+  subTitle: 'Việt Dịch: Nguyên Chánh',
+  author: '[Thiền sư] Trung Phong Minh Bổn',
+  image: '/images/teachings/zhongfeng-speeches.jpg' || '/images/teachings/zhongfeng-dharmar-speeches.jpg',
+  intro:
+    '',
+  paragraph1:
+    '',
+  paragraph2:
+    '',
+  verse:
+    '',
+  }
+export default ZhongfengSpeeches
