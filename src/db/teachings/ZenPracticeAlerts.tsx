a685b44bb01425ebdfd40f730777dcaaa4b18427
Miguel <miguel@glodival.vn>
2024-11-13 15:11:36 +0700

    update teachings

--- src/db/teachings/ZenPracticeAlerts.tsx
+++ src/db/teachings/ZenPracticeAlerts.tsx
@@ -4,11 +4,114 @@ const ZenPracticeAlerts = {
   author: '[Thiền sư] Bác Sơn Nguyên Lai',
   image: '/images/teachings/zhongfeng-speeches.jpg' || '/images/teachings/zhongfeng-dharmar-speeches.jpg',
   intro:
-    '',
+    'Giả sử cho đến chết được các thứ tâm, mà chẳng chịu hạ thủ công phu cùng với lý Pháp-thân tương ưng, ' +
+    'chẳng từng đạp lên trên cái ngạch cửa hướng thượng, ngồi ở trong thùng cơm khinh an tự tại, cái khinh an này chính là thiền bệnh. ' +
+    'Cho nên có vị Tăng hỏi Cổ-đức: Thế nào là thanh tịnh Pháp-thân ? ' +
+    'Cổ-đức đáp: Nguồn gốc của vô lượng bệnh lớn. ' +
+    'Lời này như gai gốc nuốt vào nôn ra thật khó. ' +
+    'Cổ-nhân từ trong chân tham thật ngộ, bệnh qua một phen rồi cho các Ngài duổi tay tiếp độ chẳng loại hạ kim dùi (châm chùy), ' +
+    'cần phải gặp người tuyệt khí tức, biết ngứa ngáy mới chịu chẩn bệnh. ' +
+    'Thế nên, biết bệnh mới trừ được bệnh, chữa mình rồi sau mới chữa người, đúng là người bệnh lâu năm trở thành thầy thuốc.',
   paragraph1:
-    '',
+    'CHƯƠNG I: CẢNH NGỮ KHAI THỊ NGƯỜI MỚI PHÁT TÂM THAM THIỀN',
   paragraph2:
-    '',
+    'Tham Thiền Cảnh Ngữ',
+  paragraph3:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph4:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph5:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph6:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph7:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph8:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph9:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph10:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph11:
+    'CHƯƠNG II: BÌNH LUẬN CẢNH NGỮ KHAI THỊ CỦA CỔ NHÂN',
+  paragraph12:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph13:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph14:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph15:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph16:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph17:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph18:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph19:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph20:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph21:
+    'CHƯƠNG III: CẢNH NGỮ KHAI THỊ LÚC NGHI TÌNH CHẲNG KHỞI',
+  paragraph22:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph23:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph24:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph25:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph26:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph27:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph28:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph29:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph30:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph31:
+    'CHƯƠNG IV: BÌNH LUẬN CẢNH NGỮ KHAI THỊ CỦA CỔ NHÂN',
+  paragraph32:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph33:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph34:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph35:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph36:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph37:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph38:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph39:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph40:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph41:
+    'CHƯƠNG V: CẢNH NGỮ KHAI THỊ THIỀN GIẢ THAM CÔNG ÁN',
+  paragraph42:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph43:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph44:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph45:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph46:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph47:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph48:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph49:
+    'Tham Thiền Cảnh Ngữ',
+  paragraph50:
+    'Tham Thiền Cảnh Ngữ',
   verse:
     '',
   }
--- src/db/teachings/ZhongfengSpeeches.tsx
+++ src/db/teachings/ZhongfengSpeeches.tsx
@@ -4,12 +4,131 @@ const ZhongfengSpeeches = {
   author: '[Thiền sư] Trung Phong Minh Bổn',
   image: '/images/teachings/zhongfeng-speeches.jpg' || '/images/teachings/zhongfeng-dharmar-speeches.jpg',
   intro:
-    '',
+    'Quyển TRUNG PHONG PHÁP NGỮ này được hình thành từ sự tuyển dịch một số bài pháp ngữ của Thiền-sư Trung Phong khai thị đồ chúng ' +
+    'trong bộ Thiền-sư Tạp Lục, 3 quyển, in trong Tục Tạng Kinh, tập số 122.' +
+    'Nội dung sách tấn người học lập chí lâu bền, gan dạ liều chết hạ thủ công phu tham cứu thoại đầu cho đến khi khối nghi bùng vỡ,' +
+    'hoàn toàn liễu thoát sanh tử. Vì thế, Ngài cực lực bài xích cái thói quen tai hại của người học muốn dùng tri giải để hiểu Phật pháp,' +
+    'thiền đạo, làm chướng cửa ngộ, nhất định không thể nào giải quyết được việc lớn sanh tử, mà trái lại còn thêm lớn ngã kiến, kết chặt' +
+    'gốc rễ luân hồi. Lời khai-thị thống thiết đầy nhiệt tình của Ngài có' +
+    'sức thúc giục cảnh tỉnh rất là mãnh liệt khiến người học vừa phấn chí mình, ' +
+    'vừa cảm tấm lòng từ bi chỉ dạy của Ngài, mà dốc lòng tinh tấn hành đạo.',
   paragraph1:
-    '',
+    'Trung Phong Pháp Ngữ',
   paragraph2:
-    '',
+    'Trung Phong Pháp Ngữ',
+  paragraph3:
+    'Trung Phong Pháp Ngữ',
+  paragraph4:
+    'Trung Phong Pháp Ngữ',
+  paragraph5:
+    'Trung Phong Pháp Ngữ',
+  paragraph6:
+    'Trung Phong Pháp Ngữ',
+  paragraph7:
+    'Trung Phong Pháp Ngữ',
+  paragraph8:
+    'Trung Phong Pháp Ngữ',
+  paragraph9:
+    'Trung Phong Pháp Ngữ',
+  paragraph10:
+    'Trung Phong Pháp Ngữ',
+  paragraph11:
+    'Trung Phong Pháp Ngữ',
+  paragraph12:
+    'Trung Phong Pháp Ngữ',
+  paragraph13:
+    'Trung Phong Pháp Ngữ',
+  paragraph14:
+    'Trung Phong Pháp Ngữ',
+  paragraph15:
+    'Trung Phong Pháp Ngữ',
+  paragraph16:
+    'Trung Phong Pháp Ngữ',
+  paragraph17:
+    'Trung Phong Pháp Ngữ',
+  paragraph18:
+    'Trung Phong Pháp Ngữ',
+  paragraph19:
+    'Trung Phong Pháp Ngữ',
+  paragraph20:
+    'Trung Phong Pháp Ngữ',
+  paragraph21:
+    'Trung Phong Pháp Ngữ',
+  paragraph22:
+    'Trung Phong Pháp Ngữ',
+  paragraph23:
+    'Trung Phong Pháp Ngữ',
+  paragraph24:
+    'Trung Phong Pháp Ngữ',
+  paragraph25:
+    'Trung Phong Pháp Ngữ',
+  paragraph26:
+    'Trung Phong Pháp Ngữ',
+  paragraph27:
+    'Trung Phong Pháp Ngữ',
+  paragraph28:
+    'Trung Phong Pháp Ngữ',
+  paragraph29:
+    'Trung Phong Pháp Ngữ',
+  paragraph30:
+    'Trung Phong Pháp Ngữ',
+  paragraph31:
+    'Trung Phong Pháp Ngữ',
+  paragraph32:
+    'Trung Phong Pháp Ngữ',
+  paragraph33:
+    'Trung Phong Pháp Ngữ',
+  paragraph34:
+    'Trung Phong Pháp Ngữ',
+  paragraph35:
+    'Trung Phong Pháp Ngữ',
+  paragraph36:
+    'Trung Phong Pháp Ngữ',
+  paragraph37:
+    'Trung Phong Pháp Ngữ',
+  paragraph38:
+    'Trung Phong Pháp Ngữ',
+  paragraph39:
+    'Trung Phong Pháp Ngữ',
+  paragraph40:
+    'Trung Phong Pháp Ngữ',
+  paragraph41:
+    'Trung Phong Pháp Ngữ',
+  paragraph42:
+    'Trung Phong Pháp Ngữ',
+  paragraph43:
+    'Trung Phong Pháp Ngữ',
+  paragraph44:
+    'Trung Phong Pháp Ngữ',
+  paragraph45:
+    'Trung Phong Pháp Ngữ',
+  paragraph46:
+    'Trung Phong Pháp Ngữ',
+  paragraph47:
+    'Trung Phong Pháp Ngữ',
+  paragraph48:
+    'Trung Phong Pháp Ngữ',
+  paragraph49:
+    'Trung Phong Pháp Ngữ',
+  paragraph50:
+    'Trung Phong Pháp Ngữ',
+  paragraph51:
+    'Trung Phong Pháp Ngữ',
+  paragraph52:
+    'Trung Phong Pháp Ngữ',
+  paragraph53:
+    'Trung Phong Pháp Ngữ',
+  paragraph54:
+    'Trung Phong Pháp Ngữ',
+  paragraph55:
+    'Trung Phong Pháp Ngữ',
+  paragraph56:
+    'Trung Phong Pháp Ngữ',
+  paragraph57:
+    'Trung Phong Pháp Ngữ',
+  paragraph58:
+    'Trung Phong Pháp Ngữ',
   verse:
-    '',
+    'Trung Phong Pháp Ngữ',
   }
 export default ZhongfengSpeeches
