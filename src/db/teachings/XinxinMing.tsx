ab2ec42b19e4d41a5eaa2aa2087b6387f1b666d7
Patriarchal Zen <trannhukhoinguyen@gmail.com>
2024-11-22 22:51:20 +0700

    Update XinxinMing.tsx

--- src/db/teachings/XinxinMing.tsx
+++ src/db/teachings/XinxinMing.tsx
@@ -6,43 +6,43 @@ const XinxinMing = {
   author: "[Thiền sư] Tam Tổ Tăng Xán",
   image: "/images/teachings/xinxinming-wide.jpg" || "/images/teachings/xinxinming-tall.jpg",
   intro: 
-   "",
+   '',
   verse:
-    "Thân hư huyễn trong mộng / " +
-    "Hình sắc giữa chốn không /  " +
-    "Việc đã qua không cùng / " +
-    "Việc về sau ai biết / " +
-    "Sanh đây thác về kia / " +
-    "Chìm nổi lăn lóc khổ / " +
-    "Chưa thoát ngoài ba cõi / " +
-    "Sao có thể nghỉ yên / " +
-    "Tham luyến chốn thế gian / " +
-    "Năm uẩn duyên hợp thành / " +
-    "Sanh ra mãi đến già / " +
-    "Rốt không được gì cả / " +
-    "Gốc rễ tự vô minh / " +
-    "Từ đó phải mê lầm / " +
-    "Ngày tháng luống trôi qua / " +
-    "Thời khắc không lường được / " +
-    "Một đời này uổng phí / " +
-    "Qua đời khác chẳng thông / " +
-    "Tiếp nối mãi mê lầm / " +
-    "Đều do sáu tên giặc / " +
-    "Lăn lóc giữa sáu đường / " +
-    "Loanh quanh trong ba cõi / " +
-    "Sớm cầu bậc minh sư / " +
-    "Gần gũi người đức hạnh / " +
-    "Thân tâm khéo chọn lựa / " +
-    "Quét hết bao gai góc / " +
-    "Đời nổi trôi phù phiếm / " +
-    "Sao để duyên thúc bức / " +
-    "Phải xét cùng pháp lý / " +
-    "Quyết chứng ngộ đạo mầu / " +
-    "Tâm cảnh đều mất sạch / " +
-    "Không nhớ không tưởng đến / " +
-    "Sáu căn tùy tự nhiên / " +
-    "Đi ở đều vắng lặng / " +
-    "Tâm định không sanh khởi / " +
-    "Muôn pháp tự dứt lìa",
+    'Giai nhập thử tông Đều vào tông đó
+
+Tông phi xúc diên Tông chẳng dài ngắn
+
+Nhứt niệm vạn niên Một niệm vạn năm
+'34.Vô tại bất tại Không còn chẳng còn
+
+Thập phương mục tiền Mười phương trước mắt
+
+Cực tiểu đồng đại Cực nhỏ đồng lớn
+
+Vong tuyệt cảnh giới Quên sạch cảnh giới
+35. Cực đại đồng tiểu Cực lớn đồng nhỏ
+
+Bất kiến biên biểu Chẳng thấy ngằn mé
+
+Hữu tức thị vô Có tức là không
+
+Vô tức thị hữu Không tức là có
+
+36.Nhược bất như thử Ví chẳng như vậy
+
+Tất bất tu thủ Chớ nên chần chờ
+
+Nhất tức nhất thiết Một tức tất cả
+
+Nhất thiết tức nhất Tất cả tức một.
+37. Đãn năng như thị Nếu được như vậy
+
+Hà lự bất tất Lo gì chẳng xong
+
+Tín tâm bất nhị Tin Tâm chẳng hai
+
+Bất nhị tín tâm Chẳng hai Tin Tâm. / ' +
+"38.Ngôn ngữ đạo đoạn (Lời nói dứt đạo) / ' +
+Phi cổ lai kim Chẳng xưa nay mai.',
 }
 export default XinxinMing
