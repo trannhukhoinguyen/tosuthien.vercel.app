53ac7fad233a4cadb27d9b7b75a6b5b787e9bb7b
Patriarchal Zen <trannhukhoinguyen@gmail.com>
2024-11-22 12:56:38 +0700

    Update IronFlute.tsx

--- src/db/koans/IronFlute.tsx
+++ src/db/koans/IronFlute.tsx
@@ -43,105 +43,125 @@ const IronFlute = [
   },
   {
     id: 2,
-    title: 'TẮC THỨ 2: ',
+    title: 'TẮC THỨ 2: LA SƠN KHAI ĐƯỜNG',
     image: '/images/meta-image-square-1',
     intro:
       '',
     main:
-      '',
+      'Mân Vương xây một ngôi chùa cho La Sơn và mời sư nói bài pháp đầu tiên trong pháp đường. Với tư cách là sư của viện, La Sơn lên tòa ngồi nhưng chẳng nói gì trừ hai tiếng “Cáo biệt,” trước khi trở về phòng riêng. Mân Vương tiến đến bên sư, nói, “Ngay cả những gì Phật dạy trên Thứu sơn cũng đến như hòa thượng hôm nay là cùng.” La Sơn đáp, “Tôi đã nghĩ rằng ngài là khách lạ đối với pháp nhưng bây giờ thì hóa ra ngài cũng có biết Thiền.”',
     commentBefore:
       '',
     verse:
       '',
     verseTranslated:
       '',
-    commentAfter:
-      '',
+    commentAfterVerse:
+      'Như Huyễn: Hai người này sống ở Trung Hoa vào thế kỷ thứ tám sau Tây lịch, nhưng cả hai sẽ mãi mãi cho thấy cái đẹp của sự bất toàn đối với những ai hiểu sự đánh giá bên trong. Một ngôi chùa Phật giáo luôn luôn có một tượng Phật trên bàn thờ, nhưng một ngôi chùa Thiền đích thực thì chẳng có tượng gì hết. Một vị sư đứng vào chỗ của Phật, dùng bàn thờ làm giảng tòa; ông ta mang chiếc y thừa kế từ hế hệ này đến thế hệ khác, đặt nó lên trước khi nói pháp, và lấy nó xuống ngay sau bài pháp. / ' +
+'La Sơn đã làm như vậy. Nguyên ý Hán văn nói, “Sư lên tòa, mặc y vào, cởi y ra và nói ‘Cáo biệt’. Như vậy, bài pháp của sư đã xong.” Làm hay lắm! Nhưng chớ để các ông tăng ngây thơ của ông bắt chước nhé! Nếu như thế còn tệ hơn là có tượng Phật nữa. Sự đập phá thánh tượng, mà nó bắt đầu như là một phản ứng bị nhốt trong tháp ngà. Đập nát cái tháp ngà ấy đi mới là đập phá thánh tượng thực sự. Nếu Mân Vương thuộc hàng cao nhân trong Thiền, ông ta sẽ không tỏ ra thất vọng vì đã để mất bài pháp của La Sơn, dù ông ta hiểu đúng ý chỉ im lặng của sư. Sự trưởng thành của một thiền viện cũng chậm chạp và vô hình như cây cỏ chung quanh nó. Sư, tăng và đàn-na tín thí chỉ gieo hạt và không bao giờ thấy được sự hoàn thành đầy đủ. Tại sao họ không chỉ vui vẻ hiến dâng ngày hôm nay cho thiền định? Đây là pháp mà La Sơn thừa kế từ Phật truyền xuống qua nhiều thế hệ đệ tử. / ' +
+'Lần đầu tiên khi tôi dời đến căn nhà khiêm tốn này, trong Thiền đường không có ảnh Phật, trong nhà cũng không có bàn ghế gì cả chỉ trừ chiếc ghế để ngồi chơi đàn dương cầm. Tôi chắp hai tay ngồi im lặng trên chiếc ghế này. Đó là bài pháp đầu tiên của tôi trong Thiền đường này, và từ đó tất cả những bài pháp khác chỉ là những lời giải thích mà thôi. Nếu có ai trong quí vị có ý định diễn thuyết hay viết luận giải về Thiền, hãy tự nhắc mình câu chuyện này và hãy sung sướng mà nói, “Cáo biệt” với chính mình.',
   },
   {
     id: 3,
-    title: 'TẮC THỨ 3: ',
+    title: 'TẮC THỨ 3: NAM TUYỀN PHẬT ĐÁ',
     image: '/images/meta-image-square-1',
     intro:
       '',
     main:
-      '',
-    commentBefore:
-      '',
+      'Cư sĩ Lục Cắng nói với hòa thượng Nam Tuyền, “Trong nhà con có một hòn đá hoặc ngồi hoặc nằm. Con có ý muốn chạm thành một vị Phật. Con làm được chứ?” Nam Tuyền đáp: “Được, ông làm được.” Lục Cắng lại hỏi, “Con không làm được ư?” Nam Tuyền đáp: “Không, ông không làm được.”',
+    commentBeforeVerse:
+      'Như Huyễn: Ông cư sĩ này muốn biết khả năng thành Phật của mình. Nếu không thể thành Phật, thì ông ta giống như một hòn đá.
+
+Ông nghĩ thầy ông, Nam Tuyền sẽ ca ngợi ý định tốt của ông. Nhưng Nam tuyền chỉ nói, “Được, ông làm được.” Khi Lục Cắng nói, “Con không thể làm được ư?” ý ông muốn biết chắc chắn và mong Nam Tuyền cho ông sự khẳng định. / ' +
+'Thiền của Nam Tuyền dùng lý tưởng làm cửa vào. Một người muốn chạm đá làm tượng Phật thì phải tự mình làm lấy, dù người khác có chấp thuận hay không. Nếu y có chút nghi ngờ về khả năng ấy thì y sẽ không bao giờ làm. Y bây giờ là kết quả của những gì y đã suy nghĩ và những gì y suy nghĩ bây giờ sẽ hình thành y trong tương lai. Sư hay không sư cũng không thể can thiệp được luật nhân quả này. Lục Cắng đã có một hòn đá quí, nhưng quyết tâm của ông không vững cho đến khi bị Nam Tuyền quét sạch. Giống như câu chuyện xưa về bầy chim sơn ca và anh nông dân. Chúng không được báo động khi nghe anh nông dân sẽ gặt lúa mì với sự trợ giúp của hàng xóm, nhưng khi chúng biết anh nông nông đã quyết định cắt lúa một mình không có hàng xóm trợ giúp, chúng bỏ tổ bay đi. Lục Cắng về sau trở thành một môn đệ rất giỏi.',
     verse:
       '',
     verseTranslated:
       '',
-    commentAfter:
-      '',
+    commentAfterVerse:
+      'Genro: Tôi thấy một hòn đá ông cư sĩ ấy đã mang vào viện. Tôi cũng thấy một hòn đá khác Nam Tuyền đặt trong thiền đường. Tất cả búa khắp Trung quốc không đập nát được hai hòn đá này.',
   },
   {
       id: 4,
-      title: 'TẮC THỨ 4: ',
+      title: 'TẮC THỨ 4: BÁCH LINH CHỨNG ĐẮC',
       image: '/images/iron-flute/iron-flute-cover.jpg',
       intro:
         '',
       main:
-        '',
-      commentBefore:
-        '',
+        'Bách Linh và Cư sĩ Bàng Uẩn cùng học Thiền dưới Đại sư Mã Tổ, người thừa kế Nam Nhạc. Một hôm giữa đường họ gặp nhau. Bách Linh nói, “Sư ông của chúng ta nói, ‘Nếu nói một vật tức chẳng phải’. Tôi ngờ rằng sư ông không chỉ nó cho ai hết.” Cư sĩ Bàng Uẩn nói, “Có. Sư ông đã chỉ rồi.” Bách Linh hỏi, “Cho ai vậy?” Cư sĩ lấy ngón tay tự chỉ vào mình, nói, “Cho người này.” / ' +
+'Bách Linh nói, “Sự chứng đắc của ông đẹp và sâu lắm dù cho Văn Thù và Tu Bồ Đề cũng không thể tán thán ông xứng đáng.” / ' +
+'Rồi cư sĩ nói, “Tôi ngờ rằng không có ai biết ý sư ông muốn nói gì.” Bách Linh không đáp, chỉ đội cái nón rơm lên và bỏ đi. “Đi cẩn thận,” Cư sĩ Bàng Uẩn gọi, nhưng Bách Linh cứ tiếp tục đi, không quay đầu lại.',
+      commentBeforeVerse:
+        'Như Huyễn:  Những người học Thiền đều cố gắng nhận ra Phật tánh. Họ có thể gọi nó là ‘tâm yếu’, Pháp thân, Phật thân hay ‘chơn tánh của một người’, nhưng tên chỉ là cái bóng, không phải vật thật. Như Nam Nhạc nói, “Nếu nói một vật tức chẳng nhằm.” / ' +
+'Không có gì để nghi ngờ rằng cả ông tăng và cư sĩ đã ngộ Thiền của Nam Nhạc. Tuy nhiên, vị cư sĩ vẫn còn dấu vết của sự đạt ngộ, còn ông tăng thì hoàn toàn tự do. / ' +
+'Rennyo, trụ trì chùa Bổn Nguyện, hỏi Nhất Hưu, một Thiền sư đương thời, “Tôi nghe nói rằng ông đã ngộ, có đúng vậy không?” Nhất Hưu đáp, “Tôi chưa từng làm chuyện tệ hại như vậy.” Có lẽ bày tỏ cái rạng rỡ của Thiền cho vị trụ trì ấy là vô ích, nhưng nó chứng minh hành động của Bách Linh trong câu chuyện. Đông phương có câu tục ngữ nói, “Công đâu khoe vàng với mèo.” Không nên nói chuyện Thiền với người ngoài. Vị cư sĩ trong câu truyện này đã khoe Thiền với bạn đồng học nhưng không có khoe Thiền trong cuộc sống hằng ngày. / ' +
+'Giáo sư Gronbech của trường Đại học Copenhagen tuyên bố: “Các nhà huyền học trong tất cả mọi thời đại nhất thiết diễn đạt những tư tưởng giống nhau; sự thực, giữa họ có sự đồng ý rất nhiều, họ thường dùng cùng một thứ ngôn ngữ và minh họa. Người ta có thể tìm thấy những đoạn văn đồng nhất của người Ấn độ sống hằng ngàn năm trước niên lịch của chúng ta và của các tu sĩ châu Âu thuộc thời trung cổ sau này; ngay cả bây giờ, người thi sĩ hiện đại sáng tạo những ngôn từ đưa dẫn tư tưởng thẳng đến những bản văn thời xưa. Lý do để họ đồng ý là họ có kinh nghiệm chung mà trong tự thể nó rõ ràng và chính xác như sự quan sát của người bình thường trong thế giới vật chất. Ở đây không có chỗ cho mơ mộng và ảo tưởng. Nhà huyền học bị kinh nghiệm chế ngự toàn bộ đời sống của ông ta chiếm giữ.” / ' +
+'Công án là vật kỳ lạ. Khi quí vị tham công án, nó sẽ dẫn quí vị đến thế giới của kinh nghiệm. Càng kinh nghiệm thì cái thoáng thấy Phật tánh của quí vị càng rõ ràng. Khi được cho một công án, quí vị có thể trả lời nó một cách tự nhiên như quí vị nói tên mình.',
       verse:
         '',
       verseTranslated:
         '',
-      commentAfter:
-        '',
+      commentAfterVerse:
+        'Genro: Mây nghỉ ở miệng hang / ' +
+'Suốt ngày chẳng làm gì / ' +
+'Suốt đêm trăng rọi sóng / ' +
+'Chẳng lưu dấu vết chi',
     },
   {
       id: 5,
-      title: 'TẮC THỨ 5: ',
+      title: 'TẮC THỨ 5: THIỀU SƠN MỘT CÂU',
       image: '/images/iron-flute/iron-flute-cover.jpg',
       intro:
         '',
       main:
-        '',
-      commentBefore:
-        '',
+        'Một hôm có ông tăng hỏi Thiều Sơn, “Có câu nói nào không không đúng cũng không sai chăng?” Thiều Sơn đáp, “Một mảng mây trắng chẳng khoe xấu xí.”',
+      commentBeforeVerse:
+        'Như Huyễn: Thiều Sơn thừa kế Giáp Sơn, trong giới chùa chiền ai cũng biết tính nghiêm khắc của Giáp Sơn, nhưng khi vượt qua được sự nghiêm khắc của Giáp Sơn, sư có khả nằng truyền giáo pháp thâm sâu chỉ bằng một chữ hay một câu ngắn.  / ' +
+'Ông tăng thực ra đang hỏi, “Tự tại hay giải thoát thực sự là gì?” Phần đông chúng ta vật lộn với sự vướng mắc vào phải và trái, tốt và xấu, thích và không thích mà Thiền thì siêu việt các ý nghĩ nhị nguyên này. Tuy nhiên, giây phút một người nói về Thiền của mình thì một cặp quỉ lóe mắt đằng trước y. Thiều Sơn không đá động gì đến chơn như tuyệt đối hay nhất thể cũng không dùng đến bất cứ đặc ngữ tôn giáo nào. Sư để dành những cái đó cho những người làm tôn giáo chuyên nghiệp.',
       verse:
         '',
       verseTranslated:
         '',
-      commentAfter:
-        '',
+      commentAfterVerse:
+'Genro: Không đúng cũng không sai / ' +
+Một câu cho ông đó / ' +
+Ba chục năm giữ nó / ' +
+Chớ cho ai biết nhé.',
     },
   {
       id: 6,
-      title: 'TẮC THỨ 6: ',
+      title: 'TẮC THỨ 6: ĐẦU TỬ THỌ TRAI',
       image: '/images/iron-flute/iron-flute-cover.jpg',
       intro:
         '',
       main:
-        '',
-      commentBefore:
+        'Đầu Tử được một gia đình Phật tử ở kinh đô mời ăn trưa. Chủ nhà đặt một mâm đầy cỏ trước mặt sư. Đầu Tử đặt hai nắm tay lên trán, giơ hai ngón tay cái lên làm cặp sừng. Rồi sư được người ta đem đến các món ăn thường lệ. Sau đó một ông tăng yêu cầu Đầu Tử giải thích lý do hành động kỳ lạ của sư. Đầu Tử đáp: “Quan Thế Âm Bồ-tát.”',
+      commentBeforeVersr:
         '',
       verse:
         '',
       verseTranslated:
         '',
-      commentAfter:
-        '',
+      commentAfterVerse:
+        'Như Huyễn: Không ai biết tại sao người chủ gia đình đãi cỏ Đầu Tử. Có lẽ vì ở Trung Hoa ngày xưa đôi khi người ta tin rằng một ông tăng không giác ngộ khi chết sẽ đầu thai làm con trâu. Ngày nay, nhiều tăng nhân Đông phương ngủ và ăn như trâu mà chẳng chịu khó làm việc. Đầu Tử tự trách mình thay cho đệ tử của sư, vì vậy mà đã xin lỗi thiên hạ bằng cách giả làm con trâu. Thiền tăng hay khôi hài và thường nhẹ dạ, nhận lấy trò cười và sự chế nhạo chua cay với thiện chí. Vào lúc Đầu Tử thưởng thức bữa ăn bình thường, sư quên đã quên bẵng chuyện đãi cỏ.  / ' +
+'Ông tăng, người yêu cầu Đầu Tử giải thích hành động kỳ lạ của sư, là người góp nhặt những câu dật sử. Nếu tôi là Đầu Tử, tôi sẽ không trả lời ông tăng. Nhưng Đầu Tử là người rất từ bi nên đáp, “Quan Thế Âm Bồ- tát.” / '
+'Trong kinh Pháp Hoa nói Quan Thế Âm thuyết pháp dưới nhiều hình dạng khác nhau. Một số Phật tử sợ tái sinh, chỉ nghĩ đến bản thân họ và muốn tránh nó, nhưng các vị Bồ-tát Đại thừa dạy là họ hiện ra hàng trăm hàng ngàn thân khác nhau vì hạnh phúc mỗi ngày trong tuần của chúng sinh.',
     },
   {
       id: 7,
-      title: 'TẮC THỨ 7: ',
+      title: 'TẮC THỨ 7: VÂN MÔN TIỆC Ở MIẾU THẦN',
       image: '/images/iron-flute/iron-flute-cover.jpg',
       intro:
         '',
       main:
-        '',
-      commentBefore:
-        '',
+        'Một hôm khi Vân Môn nói pháp với tăng chúng, sư hỏi, “Các ông có muốn làm quen với các Tổ, sư xưa không?” Trước khi có người trả lời, sư cầm gậy chỉ tăng chúng nói, “Chư Tổ xưa đang nhảy trên đầu các ông đó.” Rồi sư hỏi, “Các ông có muốn thấy mắt của các sư xưa không?” Sư chỉ xuống đất dưới chân tăng chúng và tự đáp, “Họ ở ngay dưới gót chân các ông đó.” Sau khi dừng lại một chút sư nói như là nói với chính mình, “Tôi đã làm tiệc ở miếu thần nhưng mấy ông thần đói chẳng thoả mãn.”',
+      commentBeforeVerse:
+        'Như Huyễn: Thật là một đoạn văn xuôi kiệt tác trong thiên hạ! Câu hỏi của Vân Môn có thể áp dụng cho những người ở châu Âu và châu Mỹ hiện nay cũng như ở Trung Hoa trong mấy thế kỷ qua. Bất cứ kẻ giả mạo nào nhắc đến sự tái sinh của các nhà sư đã trở thành rất phổ biến đến độ làm y trở thành giàu. Chỉ một mình quốc gia này thôi cũng đã có hàng ngàn người hàng ngày đang bị bọn phỉnh gạt, khuyến khích những sản phẩm tưởng tượng điên cuồng vô lý nhất lừa đảo. Nhưng việc này không thể đến độ như vậy, nếu không phải là một số người tin vào tôn giáo, nghiên cứu một triết lý, không thỏa mãn với bữa tiệc có thực. Họ giống như những ngẫu tượng trong miếu thờ thần của Trung quốc; mùi hương giác ngộ bồng bềnh chung quanh mà họ không có mũi để ngửi. Sách trong thư viện, kinh trong chùa, hàng trăm và hàng ngàn, đang toi công đãi bữa tiệc chân thực cho những ngẫu tượng có máu thịt! Rốt cuộc Vân Môn đã phí lời trong bài pháp vĩ đại của sư.',
       verse:
         '',
       verseTranslated:
         '',
-      commentAfter:
-        '',
+      commentAfterVerse:
+        'Genro: Trên đầu chỉ có một bầu trời xanh. Các Tổ, sư xưa ở đâu? Dưới chân chỉ có một quả đất lành. Mắt của các Tổ,sư xưa ở đâu? Bữa tiệc của Vân Môn chỉ là cái bóng, đâu có gì lạ, các vị thần không thể thỏa mãn cái đói của họ. Các ông muốn biết tôi làm tiệc ở miếu thần như thế nào không? Tôi đóng cửa lại, nằm dài xuống sàn nhà, sải tay duỗi chân ra, ngủ một giấc. Tại sao? Bởi vì có câu nói, “Tách đầy đến miệng không chứa thêm trà. Đất tốt chẳng từng sinh người đói.”',
     },
   {
       id: 8,
