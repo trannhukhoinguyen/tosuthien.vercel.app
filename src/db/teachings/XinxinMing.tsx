0ce87786793e47ddf510409859e6d8bf3b9ead43
Miguel <miguel@glodival.vn>
2024-11-23 04:16:24 +0700

    update masters

--- src/db/teachings/XinxinMing.tsx
+++ src/db/teachings/XinxinMing.tsx
@@ -1,48 +1,160 @@
 const XinxinMing = {
   title: "Tín Tâm Minh",
-  others: ['信心銘', 'Hsin-hsin-ming', 'Shinjinmei', 'XinxinMing']
-  youtubeId: "",
   subTitle: "Việt Dịch: Đỗ Đình Đồng",
   author: "[Thiền sư] Tam Tổ Tăng Xán",
   image: "/images/teachings/xinxinming-wide.jpg" || "/images/teachings/xinxinming-tall.jpg",
-  intro: 
+  intro:
    '',
   verse:
-    'Giai nhập thử tông Đều vào tông đó
-
-Tông phi xúc diên Tông chẳng dài ngắn
-
-Nhứt niệm vạn niên Một niệm vạn năm
-'34.Vô tại bất tại Không còn chẳng còn
-
-Thập phương mục tiền Mười phương trước mắt
-
-Cực tiểu đồng đại Cực nhỏ đồng lớn
-
-Vong tuyệt cảnh giới Quên sạch cảnh giới
-35. Cực đại đồng tiểu Cực lớn đồng nhỏ
-
-Bất kiến biên biểu Chẳng thấy ngằn mé
-
-Hữu tức thị vô Có tức là không
-
-Vô tức thị hữu Không tức là có
-
-36.Nhược bất như thử Ví chẳng như vậy
-
-Tất bất tu thủ Chớ nên chần chờ
-
-Nhất tức nhất thiết Một tức tất cả
-
-Nhất thiết tức nhất Tất cả tức một.
-37. Đãn năng như thị Nếu được như vậy
-
-Hà lự bất tất Lo gì chẳng xong
-
-Tín tâm bất nhị Tin Tâm chẳng hai
-
-Bất nhị tín tâm Chẳng hai Tin Tâm. / ' +
-"38.Ngôn ngữ đạo đoạn (Lời nói dứt đạo) / ' +
-Phi cổ lai kim Chẳng xưa nay mai.',
+    '1.Chí đạo vô nan Đạo lớn chẳng khó  / ' +
+    'Duy hiềm giản trạch Chỉ hiềm lựa chọn / ' +
+    'Đản mạc tắng ái Chẳng khởi ghét yêu / ' +
+    'Đỗng nhiên minh bạch Tự nhiên sáng rõ. / ' +
+    '2.Hào li hữu sai Sai lạc đường tơ / ' +
+    'Thiên điạ huyền cách Đất trời xa cách / ' +
+    'Dục đắc hiện tiền Muốn thấy trước mắt / ' +
+    'Mạc tồn thuận nghịch Chớ nghĩ ngược xuôi. / ' +
+    '3.Vi thuận tương tranh Thuận nghịch tranh nhau / ' +
+    'Thị vi tâm bịnh Ấy là tâm bệnh / ' +
+    'Bất thức huyền chỉ Chẳng biết mối huyền / ' +
+    'Đồ lao niệm tịnh Nhọc công nghĩ tịnh. / ' +
+    '4.Viên đồng thái hư Tròn đầy thái hư / ' +
+    'Vô khiếm vô dư Không thiếu không dư / ' +
+    'Lương do thủ xả Bởi do giữ bỏ / ' +
+    'Sở dĩ bất như Nên chẳng được như . / ' +
+    '5.Mạc trục hữu duyên Ngoài, chớ theo duyên / ' +
+    'Vật trụ không nhẫn Trong, chẳng giữ không / ' +
+    'Nhứt chủng bình hoài Một lòng bình thản  / ' +
+    'Dẫn nhiên tự tận Tự nhiên dứt bặt. / ' +
+    '6.Chỉ động qui tịnh Ngăn động cầu tịnh / ' +
+    'Chỉ cánh di động Hết ngăn động thêm / ' +
+    'Duy trệ lưỡng biên Chỉ trệ hai bên / ' +
+    'Ninh tri nhứt chủng Đâu biết một mối. / ' +
+    '7.Nhứt chủng bất thông Một mối chẳng thông / ' +
+    'Lưỡng xứ thất công Hai bên mất công / ' +
+    'Khiển hữu một hữu Đuổi có mất có / ' +
+    'Tùng không bối không Theo không phụ không. / ' +
+    '8.Đa ngôn đa lự Nói nhiều nghĩ nhiều / ' +
+    'Chuyển bất tương ưng Càng chẳng tương ưng / ' +
+    'Tuyệt ngôn tuyệt lự Dứt nói dứt nghĩ / ' +
+    'Vô xứ bất thông Chỗ nào chẳng thông. / ' +
+    '9.Qui căn đắc chỉ Về gốc được mối / ' +
+    'Tùy chiếu thất tông Theo ngọn mất tông / ' +
+    'Tu du phản chiếu Phút giây soi lại / ' +
+    ' Thắng khước tiền không Vượt liền cảnh không. / ' +
+    '10.Tiền không thiên diễn Cảnh không chuyển biến / ' +
+    'Giai do vọng kiến Đều do vọng thấy / ' +
+    'Bất dụng cầu chơn Chẳng cần cầu chơn / ' +
+    'Duy tu tức kiến Chỉ cần dứt thấy. / ' +
+    '11.Nhị kiến bất trụ Chẳng giữ thấy hai / ' +
+    'Thận vật truy tầm Cẩn thận chớ tìm / ' +
+    'Tài hữu thị phi Vừa vướng phải trái / ' +
+    'Phấn nhiên thất tâm Tâm kia cháy mất. / ' +
+    '12.Nhị do nhứt hữu Hai do một có / ' +
+    'Nhất diệc mạc thủ Một cũng chẳng giữ / ' +
+    'Nhất tâm bất sanh Một tâm chẳng sanh / ' +
+    'Vạn pháp vô cữu Vạn pháp không lỗi. / ' +
+    '13.Vô cữu vô pháp Không lỗi không pháp / ' +
+    'Bất sanh bất tâm Chẳng sanh chẳng tâm / ' +
+    'Năng tùy cảnh diệt Tâm theo cảnh diệt / ' +
+    'Cảnh trục năng trầm Cảnh theo tâm chìm. / ' +
+    '14.Cảnh do năng cảnh Cảnh do tâm khởi / ' +
+    'Năng do cảnh năng Tâm do cảnh duyên / ' +
+    'Dục tri lưỡng đoạn Muốn dứt hai bên / ' +
+    'Nguyên thị nhất không Một không đích thị. / ' +
+    '15.Nhất không đồng lưỡng Một không đồng hai / ' +
+    'Tề hàm vạn tượng Bao hàm vạn tượng / ' +
+    'Bất kiến tinh thô Chẳng thấy đục trong / ' +
+    'Ninh hữu thiên đảng Có gì sai lạc. / ' +
+    '16.Đại đạo thể khoan Đạo lớn thể khoan / ' +
+    'Vô dị vô nan Không dễ không khó / ' +
+    'Tiểu kiến hồ nghi Hiểu cạn nghi hoài / ' +
+    'Chuyển khấp chuyển trì Nhùng nhằng chẳng quyết. / ' +
+    '17.Chấp chi thất độ Chấp giữ thiên lệch / ' +
+    'Thâm nhập tà lộ Vào sâu đường tà / ' +
+    'Phóng chi tự nhiên Buông xả tự nhiên / ' +
+    'Thể vô khứ trụ Thể không đi ở. / ' +
+    '18.Nhậm tánh hiệp đạo Thuận tánh hợp đạo / ' +
+    'Tiêu dao tuyệt não Dạo chơi thoải mái / ' +
+    'Hệ niệm quai chơn Nghĩ nhiều thêm quấy / ' +
+    'Hôn trầm bất hảo Mê chìm chẳng tốt. / ' +
+    '19.Bất hảo lao thần Chẳng tốt lao thần / ' +
+    'Hà dụng sơ thân Làm gì sơ thân / ' +
+    'Dục thú nhất thừa Muốn vào nhất thừa / ' +
+    'Vật ố lục trần Sáu trần chớ ghét. / ' +
+    '20.Bất hảo lao thần Chẳng tố tlao thần / ' +
+    'Hà dụng sơ thân Làm gì sơ thân / ' +
+    'Dục thú nhất thừa Muốn vào nhất thừa / ' +
+    'Vật ố lục trần Sáu trần chớ ghét. / ' +
+    '21.Lục trần bất ố Sáu trần chẳng ghét / ' +
+    'Hoàn đồng chánh giác Tức đồng chánh giác / ' +
+    'Trí giả vô vi Kẻ trí vô vi / ' +
+    'Ngu nhơn tự phược Người ngu tự buộc. / ' +
+    '22.Pháp vô dị pháp Pháp không khác pháp / ' +
+    'Vọng tự ái trước Lầm do vướng ái / ' +
+    'Tương tâm dụng tâm Dùng tâm bắt tâm / ' +
+    'Khởi phi đại thác Phải chăng sai lớn. / ' +
+    '23.Mê sanh tịnh loạn Mê thấy tịnh loạn / ' +
+    'Ngộ vô hảo ác Ngộ không xấu tốt / ' +
+    'Nhứt thiết nhị biên Tất cả hai bên / ' +
+    'Vọng tự châm chước Do vọng châm chước. / ' +
+    '24.Mộng huyễn không hoa Hoa mộng hư không / ' +
+    'Hà lao bả tróc Nhọc công nắm bắt / ' +
+    'Đắc thất thị phi Được mất phải trái / ' +
+    'Nhứt thời phóng khước Một lần buông sạch. / ' +
+    '25.Nhãn nhược bất thùy Mắt nếu không ngủ / ' +
+    'Chư mộng tự trừ Mộng tự tiêu trừ / ' +
+    'Tâm nhược bất dị Tâm nếu chẳng khác / ' +
+    'Vạn pháp nhất như Vạn pháp như một. / ' +
+    '26.Nhất như thể huyền Nhất như thể huyền / ' +
+    'Ngột nhĩ vọng duyên Vắng bặt vọng duyên / ' +
+    'Vạn pháp tề quán Vạn pháp đồng quán / ' +
+    'Qui phục tự nhiên Trở lại tự nhiên. / ' +
+    '27.Dẫn kỳ sở dĩ Đồng quán cho nên / ' +
+    'Bất khả phương tỉ So sánh chẳng còn / ' +
+    'Chỉ động vô động Dừng động không động / ' +
+    'Động chỉ vô chỉ Động dừng không dừng. / ' +
+    '28.Lưỡng ký bất thành Hai đã chẳng được / ' +
+    'Nhất hà hữu nhĩ Giữ một làm gì / ' +
+    'Cứu cánh cùng cực Rốt ráo cùng cực / ' +
+    'Bất tồn quĩ tắc Kuôn thước còn chi / ' +
+    '29.Khế tâm bình đẳng Hợp tâm bình đẳng / ' +
+    'Sở tác câu tức Tạo tác cùng dừng / ' +
+    'Hồ nghi tận tịnh Nghi hoặc chẳng còn / ' +
+    'Chánh tín điều trực Lòng tin chánh trực. / ' +
+    '30.Nhất thiết bất lưu Tất cả chẳng còn / ' +
+    'Vô khả lý ức Không thể nhớ ghi / ' +
+    'Hư minh tự chiếu Rỗng rang tự chiếu / ' +
+    'Bất lao tâm lực Chẳng lao tâm lực. / ' +
+    '31.Phi tư lượng xứ Trí chẳng thể suy / ' +
+    'Thức tình nan trắc Tình thức khó dò / ' +
+    'Chơn như pháp giới Pháp giới chơn như / ' +
+    'Vô tha vô tự Không đây không đó / ' +
+    '32.Yếu cấp tương ưng Cần nhất tương ưng / ' +
+    'Duy ngôn bất nhị Chỉ nói chẳng hai / ' +
+    'Bất nhị giai đồng Chẳng hai nên đồng / ' +
+    'Vô bất bao dung Không gì chẳng chứa / ' +
+    '33.Thập phương trí giả Kẻ trí mười phương / ' +
+    'Giai nhập thử tông (Đều vào tông đó) / ' +
+    'Tông phi xúc diên (Tông chẳng dài ngắn) / ' +
+    'Nhứt niệm vạn niên (Một niệm vạn năm) / ' +
+    '34.Vô tại bất tại (Không còn chẳng còn) / ' +
+    'Thập phương mục tiền (Mười phương trước mắt) / ' +
+    'Cực tiểu đồng đại (Cực nhỏ đồng lớn) / ' +
+    'Vong tuyệt cảnh giới (Quên sạch cảnh giới) / ' +
+    '35. Cực đại đồng tiểu (Cực lớn đồng nhỏ) / ' +
+    'Bất kiến biên biểu (Chẳng thấy ngằn mé) / ' +
+    'Hữu tức thị vô (Có tức là không) / ' +
+    'Vô tức thị hữu (Không tức là có) / ' +
+    '36.Nhược bất như thử (Ví chẳng như vậy) / ' +
+    'Tất bất tu thủ (Chớ nên chần chờ) / ' +
+    'Nhất tức nhất thiết (Một tức tất cả) / ' +
+    'Nhất thiết tức nhất (Tất cả tức một) / ' +
+    '37. Đãn năng như thị (Nếu được như vậy) / ' +
+    'Hà lự bất tất (Lo gì chẳng xong) / ' +
+    'Tín tâm bất nhị Tin Tâm chẳng hai / ' +
+    'Bất nhị tín tâm Chẳng hai Tin Tâm. / ' +
+    '38.Ngôn ngữ đạo đoạn (Lời nói dứt đạo) / ' +
+    'Phi cổ lai kim (Chẳng xưa nay mai)',
 }
 export default XinxinMing
