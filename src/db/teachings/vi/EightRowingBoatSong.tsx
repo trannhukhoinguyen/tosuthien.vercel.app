c9e5ab3812e6aef4f5ce33054d0809d9a2347a5d
Miguel <miguel@glodival.vn>
2024-12-07 02:52:55 +0700

    update teachings

--- src/db/teachings/vi/EightRowingBoatSong.tsx
+++ src/db/teachings/vi/EightRowingBoatSong.tsx
@@ -2,11 +2,130 @@ const EightRowingBoatSong = {
   title: "BÁT TRẠO CA",
   youtubeId: "",
   subTitle: "",
-  author: "[Thiền sư] Thuyền Tử Đức Thành",
-  image: "/images/teachings/sandokai-wide.jpg" || "/images/teachings/sandokai-tall.jpg",
+  author: "[Thiền sư] Thuyền Tử Đức Thành (Chuanzi Decheng - 船子德诚)",
+  image: "https://media.tapeko.co.uk/wp-content/uploads/20240206180701/u23115p-1024x640.jpg" || "/images/teachings/sandokai-wide.jpg" || "/images/teachings/sandokai-tall.jpg",
   intro:
-   "",
+    'Thuyền Tử hòa thượng bát trạo ca (船子和尚撥棹歌) nghĩa là Bài ca chèo thuyền của Thiền sư Đức Thành (Hoa Đình Thuyền Tử) vốn là một bài cổ phong theo thể điệu ca hành.' +
+    'Sau được người đời chia thành 39 bài tương ứng với 39 đoạn khúc hợp với mỗi một ý niệm thiền học. ' +
+    'Tác phẩm mượn chuyện câu cá để ngụ ý Phật học. ' +
+    'Tác giả hóa thân thành ngư phủ, tâm hồn tiêu sái, khí tiết thanh cao, lấy việc đi câu làm nền tảng thiền lý, mượn chuyện khi gặp trên biển mà ngụ ý Phật đạo. ',
   verse:
-    '',
+    '1. 有一魚兮偉莫裁，混虛包納信奇哉，能變化，吐風雷，下線何曾釣得來。/ ' +
+    'Hữu nhất ngư hề vĩ mạc tài, hỗn hư bao nạp tín kỳ tai, năng biến hoá, thổ phong lôi, hạ tuyến hà tằng điếu đắc lai. / ' +
+    '[Dịch] Có một con cá chừ to lớn chớ giết hại, có thể dung chứa cả hư không hỗn độn thật kì lạ thay, có thể biến hóa, hô gió gọi sấm, chưa từng có ai thả mồi câu mà câu được nó lên cả. / ' +
+    '2. 千尺絲綸直下垂，一波才動萬波隨，夜靜水寒魚不食，滿船空載月明歸。/ ' +
+    'Thiên xích ty luân trực hạ thuỳ, nhất ba tài động vạn ba tuỳ, dạ tĩnh thuỷ hàn ngư bất thực, mãn thuyền không tải nguyệt minh quy. / ' +
+    '[Dịch] Nghìn thước dây tơ thả xuống dòng nước/ Một làn sóng động kéo vạn làn sóng khác động theo/ Đêm im, nước lạnh, cá chẳng cắn câu/ Thuyền không trở về chở đầy cả một thuyền trăng. / ' +
+    '3. 莫學他家弄釣船，海風起也不知邊，風拍岸，浪掀天，不易安排得帖然。/ ' +
+    'Mạc học tha gia lộng điếu thuyền, hải phong khởi dã bất tri biên, phong phách ngạn, lãng hiên thiên, bất dị an bài đắc thiếp nhiên. / ' +
+    '[Dịch] Chớ học theo người khác khua lắc thuyền câu/ Gió trên biển nổi lên thật chẳng biết đâu là bờ/ Gió vỗ vách, sóng vọt lên tới trời/ Không dễ để điều khiển sao cho thuận buồm yên ổn. / ' +
+    '4. 大釣何曾離釣求，拋竿卷線卻成愁，法卓卓，樂悠悠，自是遲疑不下鈎。/ ' +
+    'Đại điếu hà tằng ly điếu cầu, phao can quyển tuyến khước thành sầu, pháp trác trác, nhạc du du, tự thị trì nghi bất hạ câu. / ' +
+    '[Dịch] Cần câu lớn chưa từng rời miếng mồi ngon nơi lưỡi câu/ Vứt bỏ cán đi, gói ghém dây câu lại là liền sầu tủi buồn bã/ Các pháp cao siêu đằng đẵng/ Niềm vui thì dạt dào mênh mông/ Cớ sao lại tự mình trì trệ bám chấp không chịu buông lưỡi câu. / ' +
+    '5. 別人只看採芙蓉，香氣長粘繞指風，兩岸映，一船紅，何曾解染得虛空。/ ' +
+    'Biệt nhân chỉ khán thái phù dung [1], hương khí trưởng niêm nhiễu chỉ phong, lưỡng ngạn ánh, nhất thuyền hồng, hà tằng giải nhiễm đắc hư không. / ' +
+    '[Dịch] Người khác chỉ nhìn thấy ta hái hoa phù dung/ Hương thơm ngào ngạt bao quanh hướng về ngọn gió kia/ Hai bờ óng ánh/ Một con thuyền đỏ ửng/ Chưa từng có ai chịu gột rửa nhơ nhuốc để trở về với hư không. / ' +
+    '6. 靜不須禪動即禪，斷雲孤鶴兩蕭然，煙浦畔，月川前，槁木形骸在一船。/ ' +
+    'Tĩnh bất tu thiền động tức thiền, đoạn vân cô hạc lưỡng tiêu nhiên, yên phố bạn, nguyệt xuyên tiền, cảo mộc hình hài tại nhất thuyền. / ' +
+    '[Dịch] Tĩnh tại rồi thì chẳng cần thiền, tâm động thì mới cần thiền/ Hạc lẻ mây đơn trông se sắt tiêu điều/ Khói tỏa bên bờ cửa biển/ Trăng lên ở phía trước dòng sông/ Chợt thấy bóng dáng một cành củi khô nằm dựa bên một chiếc thuyền con. / ' +
+    '7. 莫道無修 便不修，菩提痴坐若為求，勤作棹，慧為舟，這個男兒始徹頭。/ ' +
+    'Mạc đạo vô tu [2] tiện bất tu, bồ đề si toạ nhược vi cầu, cần tác trạo, tuệ vi chu, giá cá nam nhi thuỷ triệt đầu. / ' +
+    '[Dịch] Chớ nên nói vô tu là không tu/ Bồ đề nếu ngồi yên mà cầu được/ Xem cần mẫn làm chèo/ Trí tuệ làm thuyền/ Ấy là người nam nhi vừa mới xuất đầu lộ diện. / ' +
+    '8. 水色春光處處新，本來不俗不同塵，着氣力，用精神，莫作虛生浪死人。/ ' +
+    'Thuỷ sắc xuân quang xứ xứ tân, bản lai bất tục bất đồng trần, trước khí lực, dụng tinh thần, mạc tác hư sinh lãng tử nhân. / ' +
+    '[Dịch] Ánh sáng mùa xuân cùng với sắc nước long lanh đâu đâu cũng đều mới mẻ thanh tân/ Vốn dĩ những thứ ấy chẳng phải là tục chẳng giống cõi trần/ Hiển lộ khí lực/ Vận dụng tinh thần/ Cho nên chớ làm người sống vô giá trị, đầu đường xó chợ. / ' +
+    '9. 獨倚蘭橈入遠灘，江花漠漠水漫漫，空釣線，渡腥羶[3]，那得凡魚總上竿。/ ' +
+    'Độc ỷ lan kiêu nhập viễn than, giang hoa mạc mạc thuỷ mạn mạn, không điếu tuyến, độ tinh thiên, na đắc phàm ngư tổng thượng can. / ' +
+    '[Dịch] Một mình ngồi tựa lan can đi vào vùng bến nước xa xôi/ Hoa sông mờ mịt nước chảy mênh mông/ Khuyết dây câu/ Băng qua mùi hôi tanh bẩn thỉu/ Thì phàm các thứ cá trên đời đều nằm hết lên trên sào câu. / ' +
+    '10. 揚卻雲蓬進卻船，一竿雲影一潭煙，既擲網，又拋筌，莫教倒被釣絲牽。/ ' +
+    'Dương khước vân bồng tiến khước thuyền, nhất can vân ảnh nhất đàm yên, ký trịch võng, hựu phao thuyên, mạc giáo đảo bị điếu ty khiên. / ' +
+    '[Dịch] Vén cỏ bồng chốn làng mây tiến vào trong thuyền/ Một phiến bóng mây một đầm khói tỏa/ Bèn ném lưới/ Lại thả nơm/ Chớ để mình ngã dây câu móc vào. / ' +
+    '11. 蒼苔滑淨坐忘機，截眼寒雲葉葉飛，戴箬笠，掛蓑衣，別無歸處是吾歸。/ ' +
+    'Thương đài hoạt tịnh toạ vong cơ, tiệt nhãn hàn vân diệp diệp phi, đới nhược lạp, quải thoa y, biệt vô quy xứ thị ngô quy. / ' +
+    '[Dịch] Rêu xanh trơn truột mướt mát gần như quên mất chuyện thế tục/ Chớp mắt nhìn áng mây lạnh từng lá bay lả tả/ Đội nón tre/ Khoác áo tơi/ Cái nơi chẳng có lối về chính là nơi ta về. / ' +
+    '12. 外卻形骸放卻情，蕭然孤坐一船輕，圓月上，四方明，不是奇人不易行。/ ' +
+    'Ngoại khước hình hài phóng khước tình, tiêu nhiên cô toạ nhất thuyền khinh, viên nguyệt thượng, tứ phương minh, bất thị kỳ nhân bất dị hành. / ' +
+    '[Dịch] Bên ngoài là hình hài, còn cái phát tiết ra ngoài là tình cảm/ Một mình ngồi trên chiếc thuyền nhẹ tiêu điều vắng vẻ/ Trăng tròn nhú lên cao/ Bốn phương sáng rực rỡ/ Nếu chẳng phải là người kì tài giỏi giắn thì không dễ dàng đi được. / ' +
+    '13. 世知我懶一何嗔，宇宙船中不管身，烈香飲，落花茵，祖師元是個閒人。/ ' +
+    'Thế tri ngã lãn nhất hà sân, vũ trụ thuyền trung bất quản thân, liệt hương ẩm, lạc hoa nhân, tổ sư nguyên thị cá gian nhân. / ' +
+    '[Dịch] Người đời biết ta lười nên có biết bao người quở trách ta/ Trong thuyền vũ trụ này chẳng kể thân mình/ Khát thì nốc bầu hương nồng/ Mỏi thì ngồi đệm hoa rơi/ Tổ sư ta vốn là người nhàn nhã như vậy. / ' +
+    '14. 都大無心罔象[4]間，此中那許是非關，山卓卓，水潺潺，忙者自忙閒者閒。/ ' +
+    'Đô đại vô tâm Võng Tượng gian, thử trung na hứa thị phi quan, sơn trác trác, thuỷ sàn sàn, mang giả tự mang gian giả gian. / ' +
+    '[Dịch] Trong lòng Võng Tượng tinh thần vô tâm bao quát rộng lớn/ Ở trong ấy có lẽ chẳng có cửa ải chắn trở/ Núi sừng sững/ Nước róc rách/ Người bận thì tự bận, người nhàn thì tự nhàn. / ' +
+    '15. 鼓棹高歌自適情，音稀和寡[5]出囂塵，清風起，浪元平，也且隨流逐勢行。/ ' +
+    'Cổ trạo cao ca tự thích tình, âm hi hoà quả xuất hiêu trần, thanh phong khởi, lãng nguyên bình, dã thả tuỳ lưu trục thế hành. / ' +
+    '[Dịch] Khua chèo cao giọng hát ca tự mình cảm thấy thích chi/ Âm thanh trong trẻo ít người hòa giọng được thoát thai khỏi nơi trần ai nhốn nháo/ Gió mát nổi lên/ Sóng trào bình nguyên/ Lại thuận theo dòng mà dâng cao. / ' +
+    '16. 浪宕從來水國間，高歌龜枕看遙山，紅蓼岸，白蘋灣，肯被蘭橈使不閒。/ ' +
+    'Lãng đãng thung lai thuỷ quốc gian, cao ca quy chẩm khán dao sơn, hồng liệu ngạn, bạch tần loan, khẳng bị lan kiêu sử bất gian. / ' +
+    '[Dịch] Trong vùng nước sâu xưa nay trôi lãng đãng/ Có kẻ kê gối rùa cao giọng hát ca ngắm nhìn núi xa/ Bên bờ núi rau đắng đỏ tươi/ Cạnh vịnh nước rau tần trắng phau/ Lại bị chiếc chèo lan khua quẩy khiến cho lòng bất nhàn. / ' +
+    '17. 一葉虛舟一副竿，瞭然無事坐煙灘，忘得喪，任悲歡，卻教人喚有多端。/ ' +
+    'Nhất diệp hư chu nhất phó can, liễu nhiên vô sự toạ yên than, vong đắc táng, nhậm bi hoan, khước giáo nhân hoán hữu đa đoan. / ' +
+    '[Dịch] Một phiến thuyền không một bộ sào/ Lòng sáng vô sự ngồi bên bờ nước khỏi lan/ Quên đi lẽ được mất/ Bỏ mặc sự bi hoan/ Nhưng lại bị người rêu là kẻ đa đoan mưu chước. / ' +
+    '18. 一任孤舟正又斜，乾坤何路指津涯，拋歲月，卧煙霞，在處江山便是家。/ ' +
+    'Nhất nhậm cô chu chính hựu tà, càn khôn hà lộ chỉ tân nhai, phao tuế nguyệt, ngoạ yên hà, tại xứ giang sơn tiện thị gia. / ' +
+    '[Dịch] Một mảnh thuyền côi thẳng rồi lại xiêu/ Càn khôn là lối nào, chỉ thấy bến nước bờ sông/ Bỏ đi ngày tháng/ Nằm giữa ráng trời/ Xem chốn sông núi xa xôi là nhà của mình. / ' +
+    '19. 愚迷未識主人翁，終日孜孜恨不同，到彼岸，出樊籠，元來只是舊時公。/ ' +
+    'Ngu mê vị thức chủ nhân ông, chung nhật tư tư hận bất đồng, đáo bỉ ngạn, xuất phiền lung, nguyên lai chỉ thị cựu thì công. / ' +
+    '[Dịch] Ông chủ nhân là người u mê chưa tỏ ngộ/ Suốt ngày không ngớt niềm uất hận/ (Ngày nọ) Đến bờ bên kia/ Ra khỏi chiếc lồng giậu/ Hóa ra chỉ là một kẻ của thời trước (trước đây). / ' +
+    '20. 古釣先生鶴髮垂，穿波出浪不曾疑，心蕩蕩，笑怡怡，長道無人畫得伊。/ ' +
+    'Cổ Điếu tiên sinh hạc phát thuỳ, xuyên ba xuất lãng bất tằng nghi, tâm đãng đãng, tiếu di di, trường đạo vô nhân hoạch đắc y. / ' +
+    '[Dịch] Tiên sinh Cổ Điếu (Chiếc cần câu cũ) tóc hạc rủ xuống/ Băng sóng đáp nước chưa từng chùn bước/ Lòng rộng thênh thang/ Cười vui khoái trá/ Đường xa chẳng thấy ai họa lại được ông. / ' +
+    '21. 一片江雲倐忽開，翳空朗日若為哉，適消散，又徘徊，試問本從何處來。/ ' +
+    'Nhất phiến giang vân thúc hốt khai, ế không lãng nhật nhược vi tai, thích tiêu tán, hựu bồi hồi, thí vấn bản thung hà xứ lai. / ' +
+    '[Dịch] Một phiến mây hiện ra chớp nhoáng trên sông/ Thể như muốn ẩn vào hư không nấp vào vầng dương sáng sủa/ Vừa mới tiêu tán, lại lưỡng lự chần chừ/ Thử hỏi xưa nay đâu là chốn về. / ' +
+    '22. 不妨輪線不妨鈎，只要鈎輪得自由，擲即擲，收即收，無蹤無跡樂悠悠。/ ' +
+    'Bất phương luân tuyến bất phương câu, chỉ yếu câu luân đắc tự do, trịch tức trịch, thu tức thu, vô tung vô tích nhạc du du. / ' +
+    '[Dịch] Chẳng ngại tơ quấn chẳng ngại câu/ Chỉ cần câu xoay vòng nắm được sự tự do/ Ném thì ném/ Thu thì thu/ Chẳng có dấu tích niềm vui dằng dặc bất tận. / ' +
+    '23. 釣下俄逢赤水珠，光明圓澈等清虛，靜即出，覓還無，不在鱺龍[6]不在魚。/ ' +
+    'Điếu hạ nga phùng Xích Thuỷ châu, quang minh viên triệt đẳng thanh hư, tĩnh tức xuất, mịch hoàn vô, bất tại Ly Long bất tại ngư. / ' +
+    '[Dịch] Dưới cần nghiêng vớ được ngọc Xích Thủy (ngọc đen)/ Quang minh tròn vạnh ngang với thanh hư (đạo huyền vi)/ Lúc tĩnh thì trồi lên/ Nhưng tìm thì chẳng có/ Không nằm ở trong miệng loại Li Long cũng chẳng nằm trong bụng cá. / ' +
+    '24. 卧海拏雲勢莫知，優遊何處不相宜，香象[7]子，大龍兒，甚麼波濤颺得伊。/ ' +
+    'Ngoạ hải nã vân thế mạc tri, ưu du hà xứ bất tương nghi, Hương Tượng tử, Đại Long nhi, thậm ma ba đào dương đắc y. / ' +
+    '[Dịch] Nằm giữa biển vọt trời mây thế nào chẳng biết/ Ngao du tự tại nơi nào chẳng hợp/ Ngài Hương Tượng, con Đại Long/ Đâu ngọn ba đào nào dâng cao được tới những người ấy. / ' +
+    '25. 雖募求魚不食魚，網壺蓬戶本空無，在世界，作凡夫，知聞只是個毗盧[8]。/ ' +
+    'Tuy mộ cầu ngư bất thực ngư, võng hồ bồng hộ bản không vô, tại thế giới, tác phàm phu, tri văn chỉ thị cá bì lô. / ' +
+    '[Dịch] Tuy mong bắt được cá nhưng không ăn cá/ Nhà tranh bầu cá vốn chẳng có gì/ Ở trong thế giới này/ Làm kẻ phàm phu/ Người nghe được biết được (chỉ người trí tuệ) chỉ có ngài Tỳ Lô. / ' +
+    '26. 香餌針頭也不無，向來只是釣名魚，波沃日，浪涵虛，萬象籮籠號有餘。/ ' +
+    'Hương nhị châm đầu dã bất vô, hướng lai chỉ thị điếu danh ngư, ba ốc nhật, lãng hàm hư, vạn tượng la lung hiệu hữu dư. / ' +
+    '[Dịch] Mồi thơm câu sắc cũng không phải không có/ Xưa nay chỉ muốn câu được cá hiếm cá quý/ Sóng vọt trời/ Sóng chứa hư không/ Vạn vật nếu cho vào lồng vẫn còn dư. / ' +
+    '27. 乾坤為舸月為蓬，一屏雲山一罨風，身放蕩，性靈空，何妨南北與西東。/ ' +
+    'Càn khôn vi khả nguyệt vi bồng, nhất bình vân sơn nhất yểm phong, thân phóng đãng, tính linh không, hà phương nam bắc dữ tây đông. / ' +
+    '[Dịch] Càn khôn là thuyền cả, trăng là tóc bồng/ Một bức núi mây một màn gió/ Thân như phóng đãng/ Tính tựa trời cao/ Có gì phải ngại Nam Bắc hay Đông Tây. / ' +
+    '28. 終日江頭理棹間，忽然失濟若為還，灘急急，水潺潺，爭把浮生作等閒。/ ' +
+    'Chung nhật giang đầu lý trạo gian, hốt nhiên thất tế nhược vi hoàn, than cấp cấp, thuỷ sàn sàn, tranh bả phù sinh tác đẳng gian. / ' +
+    '[Dịch] Trọn ngày xuôi chèo ở đầu sông/ Đột nhiên chưa qua được bờ bên kia đã phải quay lại/ Bến nước chảy gấp gấp/ Nước bể chảy xiên xiết/ Tranh lấy kiếp phù sinh chỉ để làm kẻ bình thường. / ' +
+    '29. 有鶴翱翔四海風，往來蹤跡在虛空，圖不得，算何窮，日月還教沒此中。/ ' +
+    'Hữu hạc ngao tường tứ hải phong, vãng lai tung tích tại hư không, đồ bất đắc, toán hà cùng, nhật nguyệt hoàn giáo một thử trung. / ' +
+    '[Dịch] Kìa cánh hạc bay lượn trong bốn bể/ Xưa nay tung tích đều nằm ở hư không/ Tính chẳng đẳng/ Toán chẳng cùng/ Nhật nguyệt quần xoay lặn mất trong chốn này. / ' +
+    '30. 釣頭曾未曲些些，靜向江濱度歲華，酌山茗，折蘆花，誰言埋沒在煙霞。/ ' +
+    'Điếu đầu tằng vị khúc ta ta, tĩnh hướng giang tân độ tuế hoa, chước sơn minh, chiết lô hoa, thuỳ ngôn mai một tại yên hà. / ' +
+    '[Dịch] Kìa lưỡi câu thẳng chưa từng uốn bẻ/ Lặng nhìn bến sông đã trải bao mùa/ Rót trà ngon trên núi/ Bẻ ngắt ngọn bông lau/ Ai người nói rằng đời vùi chôn trong khói lam ráng chiều (ý nói người đời cười chê ta sống đời ẩn cư, vùi mình nơi heo hút). / ' +
+    '31. 吾自無心無事間，此心只有水雲關，攜釣竹，混塵寰，喧靜都來離又閒。/ ' +
+    'Ngô tự vô tâm vô sự gian, thử tâm chỉ hữu thuỷ vân quan, huề điếu trúc, hỗn trần hoàn, huyên tĩnh đô lai ly hựu gian. / ' +
+    '[Dịch] Tự ta vô tâm trong cõi vô sự/ Tâm này chỉ có cửa mây nước mà thôi/ Dắt cần trúc/ Khuấy hồng trần/ Tĩnh náo lũ lượt kéo tới, bỏ đi hết thì liền nhàn thôi. / ' +
+    '32. 晴川清瀨水橫流，蕭灑元同不繫舟，長自在，恣優遊，將心隨逐幾時休。/ ' +
+    'Tình xuyên thanh lai thuỷ hoành lưu, tiêu sái nguyên đồng bất hệ chu, trưởng tự tại, tứ ưu du, tướng tâm tuỳ trục kỷ thì hưu. / ' +
+    '[Dịch] Đồng quang, thác lặng, nước chảy ngang/ Tấm lòng tiêu sái như thuyền không buộc/ Thường tự tại, mặc tiêu diêu/ Mang tâm theo đuổi (chấp trước) thì biết khi nào dừng. / ' +
+    '33. 歐冶 [9],銑鋒價最高，海中收得用吹毛，龍鳳繞，鬼神號，不見全牛可下刀。[10]/ ' +
+    'Âu Dã tiển phong giá tối cao, hải trung thu đắc dụng xuy mao, long phượng nhiễu, quỷ thần hiệu, bất kiến toàn ngưu khả hạ đao. / ' +
+    '[Dịch] Lưỡi gươm Âu Dã giá cao vời/ Ta nhặt được dưới lòng bể chỉ dùng để phủi lông thôi/ Long phụng quây quần/ Quỷ thần gào thét/ Không thấy cả trâu mới xuống dao được. / ' +
+    '34. 動靜由來兩本空，誰教日月強施功，波渺渺，霧濛濛，卻成江上隱雲中。/ ' +
+    'Động tĩnh do lai lưỡng bản không, thuỳ giáo nhật nguyệt cường thi công, ba diêu diêu, vụ mông mông, khước thành giang thượng ẩn vân trung. / ' +
+    '[Dịch] Tĩnh động xưa nay vốn là không/ Ai rằng nhật nguyệt đòi tranh công/ Sóng dằng dặc/ Sương mịt mờ/ Lại tỏa trên dòng sông ẩn vào trong mây. / ' +
+    '35. 問我生涯只是船，子孫各自睹機緣，不由地，不由天，除卻蓑衣無可傳。/ ' +
+    'Vấn ngã sinh nhai chỉ thị thuyền, tử tôn các tự đổ cơ duyên, bất do địa, bất do thiên, trừ khước thoa y vô khả truyện. / ' +
+    '[Dịch] Có người hỏi ta kiếp người chỉ là con thuyền/ Con cháu từng người âu cũng chỉ là cơ duyên/ Không phải từ đất, cũng chẳng phải do trời/ Ngoài manh áo rách thì chẳng có gì để truyền lại. / ' +
+    '36. 媚俗無機獨任真，何須洗耳[11]復澄神[12]，雲與月，友兼親，敢向浮漚任此身。/ ' +
+    'Mị tục vô cơ độc nhậm chân, hà tu tẩy nhĩ phục trừng thần, vân dữ nguyệt, hữu kiêm thân, cảm hướng phù ẩu nhậm thử thân. / ' +
+    '[Dịch] Hùa theo thế tục chẳng có điểm dừng chi bằng tự mình nghe theo tự nhiên/ Hà tất gì phải rửa tai rồi lắng thần (tập trung)/ Xem mây và trăng/ Là bạn bè với người thân/ Mới dám để mặc thân này cho bọt nước. / ' +
+    '37. 逐塊追歡[13]不識休，津樑混不掛心頭，霜葉落，岸花秋，卻教漁父為人愁。/ ' +
+    'Trục khối truy hoan bất thức hưu, tân lương hỗn bất quải tâm đầu, sương diệp lạc, ngạn hoa thu, khước giáo ngư phụ vi nhân sầu. / ' +
+    '[Dịch] Tìm vui chẳng biết dừng như con chó đuổi theo hòn đất/ Bờ bến cuồn cuộn nhưng lòng không lo nghĩ/ Lá rơi trong sương/ Hoa thu bên bờ/ Lại khiến cho người ngư phụ vì người mà sầu khổ. / ' +
+    '38. 二十年來江上游，水清魚見不吞鈎，釣竿斫盡重栽竹，不計工程得便休。/ ' +
+    'Nhị thập niên lai giang thượng du, thuỷ thanh ngư kiến bất thôn câu, điếu can chước tận trùng tài trúc, bất kế công trình đắc tiện hưu. / ' +
+    '[Dịch] Lênh đênh trên dòng sông đã chừng hai mươi năm nay/ Nước trong thấy cả cá nhưng cá không đớp mồi câu/ Cán câu gãy đứt hết rồi thì lại trông thêm cây trúc/ Chẳng kể công lao, bắt được cá mới thôi. / ' +
+    '39. 三十餘年坐釣台，鈎頭往往得黃能[14]，錦鱗不遇虛勞力，收取絲綸歸去來。/ ' +
+    'Tam thập dư niên toạ điếu đài, câu đầu vãng vãng đắc hoàng năng, cẩm lân bất ngộ hư lao lực, thu thủ ty luân quy khứ lai. / ' +
+    '[Dịch] Đã ba mươi năm nay ngồi trên bệ câu/ Đầu cần câu thường hay câu được Hoàng Năng/ Chẳng gặp được loài cá quý nên làm hao tốn sức lực/ Cho nên bèn thâu dọn tơ câu rồi quay trở về.',
 }
 export default EightRowingBoatSong
