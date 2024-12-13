6fd7505c36647ee9dc7c8911822f97612c540853
Miguel <miguel@glodival.vn>
2024-12-13 12:03:03 +0700

    update domain switching

--- middleware.mjs
+++
@@ -1,6 +0,0 @@
-export async function middleware(req) {
-  const lang = req.headers.get('accept-language') || 'en';
-  const targetDomain = lang.startsWith('vi') || !lang ? 'https://tosuthien.quest' : 'https://patriarchalzen.quest';
-
-  return Response.redirect(targetDomain + req.url);
-}
