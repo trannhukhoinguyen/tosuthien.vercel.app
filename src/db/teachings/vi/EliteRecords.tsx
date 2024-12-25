98741da88c4464fb0709d25b654e844f552114d5
trann <trannhukhoinguyen@gmail.com>
2024-12-25 13:52:36 +0700

    update

---
+++ src/db/teachings/vi/EliteRecords.tsx
@@ -0,0 +1,39 @@
+const EliteRecords = [
+  {
+    id: 1,
+    title: "TS. HOÀI NHƯỢNG - Nam Nhạc",
+    author: "",
+    anchor: "",
+    image: "/images/meta-image-square-1.png",
+    content:
+      "",
+  },
+  {
+    id: 2,
+    title: "",
+    author: "",
+    anchor: "",
+    image: "/images/meta-image-square-2.png",
+    content:
+      "",
+  },
+  {
+    id: 3,
+    title: "",
+    author: "",
+    anchor: "",
+    image: "/images/meta-image-square-3.png",
+    content:
+      "",
+  },
+  {
+    id: 4,
+    title: "",
+    author: "",
+    anchor: "",
+    image: "/images/meta-image-square-1.png",
+    content:
+      "",
+  },
+]
+export default EliteRecords
