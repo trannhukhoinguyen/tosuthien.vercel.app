d1073b9e73235f88aeb31e9fabfd12e35a56ab6c
trann <trannhukhoinguyen@gmail.com>
2024-12-28 10:47:12 +0700

    update teachings

---
+++ src/db/teachings/vi/ZhaoLun.tsx
@@ -0,0 +1,20 @@
+const ZhaoLun = {
+  title: "Triệu Luận",
+  youtubeId: "",
+  subTitle: "Việt Dịch: [Đại sư] Thích Nhuận Châu",
+  author: "[Đại sư] Tăng Triệu",
+  image: "/images/teachings/zhao-lun-wide.jpg" || "/images/teachings/zhao-lun-tall.jpg",
+  intro:
+    "",
+  paragraph1:
+    "",
+  paragraph2:
+    "",
+  paragraph3:
+    "",
+  paragraph4:
+    "",
+  verse:
+    "",
+}
+export default ZhaoLun
---
+++ src/db/teachings/vi/ZhaoLunBriefExplanation.tsx
@@ -0,0 +1,20 @@
+const ZhaoLunBriefExplanation = {
+  title: "Triệu Luận Lược Giải",
+  youtubeId: "VGKWTiLiThA",
+  subTitle: "Việt Dịch: [Đại sư] Thích Duy Lực",
+  author: "[Đại sư] Hám Sơn",
+  image: "/images/teachings/zhao-lun-brief-explanation-wide.jpg" || "/images/teachings/zhao-lun-brief-explanation-tall.jpg",
+  intro:
+    "",
+  paragraph1:
+    "",
+  paragraph2:
+    "",
+  paragraph3:
+    "",
+  paragraph4:
+    "",
+  verse:
+    "",
+}
+export default ZhaoLunBriefExplanation
