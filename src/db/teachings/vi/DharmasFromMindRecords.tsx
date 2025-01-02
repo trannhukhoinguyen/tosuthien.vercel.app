a453b53f99a046b9b58daec1226f01b7de529645
trann <trannhukhoinguyen@gmail.com>
2025-01-02 16:54:14 +0700

    add teachings

---
+++ src/db/teachings/vi/DharmasFromMindRecords.tsx
@@ -0,0 +1,43 @@
+const DharmasFromMindRecords = [
+  {
+    id: 1,
+    title: "",
+    author: "",
+    anchor: "",
+    color: "blue",
+    image: "/images/meta-image-square-1.png",
+    content:
+      "",
+  },
+   {
+    id: 2,
+    title: "",
+    author: "",
+    anchor: '',
+    color: "yellow",
+    image: "/images/meta-image-square-1.png",
+    content:
+      "",
+  },
+   {
+    id: 3,
+    title: "",
+    author: "",
+    anchor: '',
+    color: "yellow",
+    image: "/images/meta-image-square-1.png",
+    content:
+      "",
+  },
+ {
+    id: 4,
+    title: "",
+    author: "",
+    anchor: '',
+    color: "yellow",
+    image: "/images/meta-image-square-1.png",
+    content:
+      "",
+  },
+]
+export default DharmasFromMindRecords
