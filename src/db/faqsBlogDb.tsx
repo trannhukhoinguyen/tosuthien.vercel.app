3e4e00ba5b29e22151073f61343599dc0e1efddd
trann <trannhukhoinguyen@gmail.com>
2025-01-03 13:25:00 +0700

    update blogs

---
+++ src/db/faqsBlogDb.tsx
@@ -0,0 +1,109 @@
+const faqsBlogDb = [
+  {
+    id: 1,
+    ask: '1. Người thế tục hỏi: Tôi muốn thường ăn chay, có công đức chăng?',
+    type: 'vegetarianism',
+    reply:
+      'Thiền sư đáp: Các loài có vẩy, có mai, có lông mao, lông vũ, thể chất nó tuy khác với người, nhưng sự hiểu biết và sự đau khổ đâu khác chi người. ' +
+      'Bởi đời trước nghĩ sai, nên đời này hình hài có khác. Nếu như một lòng tín thành dứt ăn thịt, giữ giới sát, thật là bậc “Đại đức quân tử”. ' +
+      'Có thể bảo là người nhân từ, người thiện. ' +
+      'Nếu quả ông ăn chay, giữ giới suốt đời không sờn lòng, chẳng những thêm nhiều phúc và thọ mà còn gieo được chánh nhơn giác ngộ. ' +
+      'Khá thương cho người đời không trí huệ, tham đắm vị ngon mặc tình đồn chứa, ra tay sát hạt, tạo tác muôn ngàn. ' +
+      'Chỉ ngon qua ba tấc lưỡi, một ngày kia cải đầu đổi mặt, lần lượt nuốt ăn. Lúc ấy đau khổ hối hận không người giúp cho.',
+  },
+  {
+    id: 2,
+    ask: '2. Người đời đều cho heo, dê, gà, ngỗng, các loài có vẩy, có mai, có lông mao, lông vũ... trời sanh ra các loài này để làm đồ ăn cho người. ' +
+      'Nếu không giết để ăn, dùng chúng làm việc gì?',
+    type: 'vegetarianism',
+    reply:
+      'Người đời ăn thịt đương nhiên, buông lung tâm ý mổ giết để thích khẩu, khoái bụng. ' +
+      'Đâu chẳng biết, tất cả chúng sanh dưới nước, trong đất, trên không, trên bộ, chẳng quan là do nghiệp lực quả báo mà có thân xác khác nhau. ' +
+      'Người đời tuy nhờ phước nhứt thời mà khinh thường sát hại quá nhiều rồi. ' +
+      'Tha lực yếu kém làm sao chống chỏi nổi hận oán kia? ' +
+      'Có ngày sẽ báo đền oan trái. ' +
+      'Quả thật nếu heo, dê... trời sanh ra làm đồ ăn cho người, còn ở thế gian những loài sài lang, hổ báo, muỗi mòng, chí rận... vô hạn ác thú hung cầm đều hay ăn người, đâu không phải trời sanh? ' +
+      'Giả sử người nam kể nữ đều làm đồ ăn cho loài vật có nên không? ' +
+      'Người đời không rõ tạo nghiệp sẽ có thường mạng lẫn nhau. Không biết cớ sao lại cho việc sát sanh là việc đáng nên làm, thật quá ngu muội.',
+  },
+  {
+    id: 3,
+    ask: '3. Tôi thường nghe người đời có nói: “Người nhiều lòng tốt cũng như ăn chay rồi”. Lý này thế nào?',
+    type: 'vegetarianism',
+    reply:
+      'Bậc nhân đức quân từ, người hiền có lòng nhơn từ, làm lợi ích cho người, cứu tế loài vật, nhóm công chứa đức, cho đến những chỗ động dụng hay chỗ làm việc, thấy sanh linh rơi vào chỗ chết, lúc ấy đâu không khởi lòng trắc ẩn cứu cho được sống, có lòng từ bi như thế mới bảo là người tốt. ' +
+      'Vô cớ giết nó cho vào miệng ta, hoặc cầm dao bén mổ bụng, hoặc nắm dao nhỏ đâm vào tim, lóc da, đánh vẩy, cắt cổ, bổ vỏ hay vào lò nướng, trăm cách tạo tác làm cho ngon miệng, khoái bụng, lòng thương xót hoàn toàn không, như vậy lòng tốt ở chỗ nào? mà lại nói lời mê như thế? ' +
+      'Tội lỗi kia làm sao có thể sám hối? Tôi nay nói rõ ông hãy lắng nghe: ' +
+      'Quả như ông một đời giữ giới sát, ăn chay suốt đời chẳng chán, dù tâm ông chẳng tốt, phải đoạ vào tay người giết, hoặc trong vạc sôi lò lửa.',
+  },
+  {
+    id: 4,
+    ask: '4. ',
+    reply:
+      '',
+  },
+  {
+    id: 5,
+    ask: '5. ',
+    reply:
+      '',
+  },
+  {
+    id: 6,
+    ask:
+      '6. ',
+    reply:
+      '',
+  },
+  {
+    id: 7,
+    ask:
+      '7. ',
+    reply:
+      ''
+  },
+  {
+    id: 8,
+    ask:
+    '8. Đường lối Tổ sư thiền là nghĩ đến câu thoại đầu phải không?',
+    reply:
+    ''
+  },
+  {
+    id: 9,
+    ask:
+      '9. Tâm không định làm sao tham thiền được?',
+    reply:
+      ''
+  },
+  {
+    id: 10,
+    ask:
+      '10. Hỏi câu thoại đầu muốn hiểu, giải đáp ra thì như thế nào?',
+    reply:
+      '',
+  },
+  {
+    id: 11,
+    ask:
+      '11.',
+    reply:
+      ''
+  },
+  {
+    id: 12,
+    ask:
+      '12. Khi mình đọc thoại đầu, bổng nhiên không còn nghi tình, mà nó giống như qua đi qua đi và trở thành trạng thái niệm thoại đầu. / ' +
+      'Như trong trường hợp đó phải như thế nào?',
+    reply:
+      ''
+  },
+  {
+    id: 13,
+    ask:
+      '13. Tham thoại đầu thường hay tìm ra giải pháp để trả lời câu thoại, làm sao để tránh trường hợp đó xảy ra',
+    reply:
+      '',
+  }
+]
+export default faqsBlogDb;
