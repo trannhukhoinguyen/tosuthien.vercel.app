1a1587267ab11f149f5ded0400211b4189c7f144
Patriarchal Zen <trannhukhoinguyen@gmail.com>
2025-02-09 22:38:40 +0700

    Update DeshanXuanjian.tsx

--- src/db/speeches/DeshanXuanjian.tsx
+++ src/db/speeches/DeshanXuanjian.tsx
@@ -1,3 +1,61 @@
-const DeshanXuanjian = [];
+const DeshanXuanjian = [
+{
+    id: 1,
+    authorName: 'Deshan Xuanjian',
+    authorImage: '/images/wide/cloudy-mountain-peak.jpg',
+    authorLink: '/masters/Deshan-Xuanjian',
+    speechMain:
+      'aaaa / ' +
+      'aaa / ' +
+      'aa / ' +
+      'a',
+    speechType: '',
+  },
+  {
+    id: 2,
+    authorName: 'Deshan Xuanjian',
+    authorImage: '/images/wide/cloudy-mountain-peak.jpg',
+    authorLink: '/b',
+    speechMain:
+      'bbbb / bbb / bb / b',
+    speechType: '',
+  },
+  {
+    id: 3,
+    authorName: 'Deshan Xuanjian',
+    authorImage: '/images/wide/cloudy-mountain-peak.jpg',
+    authorLink: '/c',
+    speechMain:
+      'cccc / ccc / cc / c',
+    speechType: '',
+  },
+  {
+    id: 4,
+    authorName: 'Deshan Xuanjian',
+    authorImage: '/images/wide/cloudy-mountain-peak.jpg',
+    authorLink: '/d',
+    speechMain:
+      'dddd / ddd / dd / d',
+    speechType: '',
+  },
+  {
+    id: 5,
+    authorName: 'Deshan Xuanjian',
+    authorImage: '/images/wide/cloudy-mountain-peak.jpg',
+    authorLink: '/e',
+    speechMain:
+      'eeee / eee / ee / e',
+    speechType: '',
+  },
+  {
+    id: 6,
+    authorName: 'Deshan Xuanjian',
+    authorImage: '/images/wide/cloudy-mountain-peak.jpg',
+    authorLink: '/f',
+    speechMain:
+      'ffff / fff / ff / f',
+    speechType: '',
+  },
+];
 
-export DeshanXuanjian;
+export default DeshanXuanjian;
