5e361e8740163ca5465dd8ff7f152c91e4a793ec
Patriarchal Zen <trannhukhoinguyen@gmail.com>
2025-02-09 22:42:26 +0700

    Update speechDb.tsx

--- src/db/speechDb.tsx
+++ src/db/speechDb.tsx
@@ -1,60 +1,6 @@
+import DeshanXuanjian from "../../db/speeches/DeshanXuanjian.tsx";
+
 const speechDb = [
-  {
-    id: 1,
-    authorName: 'Deshan Xuanjian',
-    authorImage: '/images/wide/cloudy-mountain-peak.jpg',
-    authorLink: '/masters/Deshan-Xuanjian',
-    speechMain:
-      'aaaa / ' +
-      'aaa / ' +
-      'aa / ' +
-      'a',
-    speechType: '',
-  },
-  {
-    id: 2,
-    authorName: '2222',
-    authorImage: '/images/wide/cloudy-mountain-peak.jpg',
-    authorLink: '/b',
-    speechMain:
-      'bbbb / bbb / bb / b',
-    speechType: '',
-  },
-  {
-    id: 3,
-    authorName: '3333',
-    authorImage: '/images/wide/cloudy-mountain-peak.jpg',
-    authorLink: '/c',
-    speechMain:
-      'cccc / ccc / cc / c',
-    speechType: '',
-  },
-  {
-    id: 4,
-    authorName: '4444',
-    authorImage: '/images/wide/cloudy-mountain-peak.jpg',
-    authorLink: '/d',
-    speechMain:
-      'dddd / ddd / dd / d',
-    speechType: '',
-  },
-  {
-    id: 5,
-    authorName: '5555',
-    authorImage: '/images/wide/cloudy-mountain-peak.jpg',
-    authorLink: '/e',
-    speechMain:
-      'eeee / eee / ee / e',
-    speechType: '',
-  },
-  {
-    id: 6,
-    authorName: '6666',
-    authorImage: '/images/wide/cloudy-mountain-peak.jpg',
-    authorLink: '/f',
-    speechMain:
-      'ffff / fff / ff / f',
-    speechType: '',
-  },
+  ...DeshanXuanjian,
 ]
 export default speechDb;
