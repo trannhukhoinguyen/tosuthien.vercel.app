05d8539500d845f80029aa15e6dea9415b8efc2f
Patriarchal Zen <trannhukhoinguyen@gmail.com>
2025-02-10 21:24:14 +0700

    Update speechDb.tsx

--- src/db/speechDb.tsx
+++ src/db/speechDb.tsx
@@ -1,6 +1,8 @@
 import DeshanXuanjian from "../db/speeches/DeshanXuanjian.tsx";
+import NanyangHuizhong from "../db/speeches/NanyangHuizhong.tsx";
 
 const speechDb = [
   ...DeshanXuanjian,
+...NanyangHuizhong,
 ]
 export default speechDb;
