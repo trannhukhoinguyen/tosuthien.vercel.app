b699c076ae9cd416ec18092e716f031edcdd274d
Patriarchal Zen <trannhukhoinguyen@gmail.com>
2025-02-10 20:43:50 +0700

    Update DeshanXuanjian.tsx

--- src/db/speeches/DeshanXuanjian.tsx
+++ src/db/speeches/DeshanXuanjian.tsx
@@ -7,16 +7,16 @@ const DeshanXuanjian = [
     speechMain:
       'Chớ nên động dụng tâm tư, muôn kiếp ngàn đời luân hồi trong tam giới là vì các ông có tâm tu cầu. Vì cớ sao? Tâm sanh thì các thứ pháp sanh. ' +
       'Nếu một niệm chẳng sanh thì hằng thoát khỏi sanh tử, không còn bị sanh tử trói buộc, ưng đi thì đi, ưng ngồi thì ngồi, tự do tự tại, lại còn có việc gì để làm? / ' +
-      'Các ông chớ nên cuồng vọng. Ta khuyên các ông thôi đi, dừng đi.' +
+      'Các ông chớ nên cuồng vọng. Ta khuyên các ông thôi đi, dừng đi. ' +
       'Các ông vừa khởi một niệm đó là quyến thuộc của ác ma rồi, cũng chính là kẻ phàm phu tục tử phá giới. / ' +
-      'Nhân giả, từ nhóm này đến nhóm khác kéo nhau đi hành cước, nói “ta hiểu thiền hiểu đạo.' +
+      'Nhân giả, từ nhóm này đến nhóm khác kéo nhau đi hành cước, nói “ta hiểu thiền hiểu đạo. ' +
       'Vỗ hông vỗ ngực, đến đâu cũng khoe khoang, nhưng đến đây phải mửa hết mới được vô sự.' +
-      'Các ông chỉ cần bên ngoài không chấp thanh sắc, bên trong không có bất cứ tri thức kiến giải gì.' +
+      'Các ông chỉ cần bên ngoài không chấp thanh sắc, bên trong không có bất cứ tri thức kiến giải gì. ' +
       'Bản thể không phân thánh phàm, lại còn học cái gì? ' +
       'Giả sử học được trăm ngàn diệu nghĩa cũng chỉ là con quỉ ăn ung mủ, tất cả đều là tinh hồn quỉ mị. / ' +
       'Các ông chớ nên mến thánh, thánh là tên suông. ' +
       'Ở trong ba cõi mười phương, nếu có một hạt bụi một pháp để cho các ông cầu được, cho các ông chấp trước, sanh tri giải, giữ gìn để làm vật quý trọng, đều rơi vào thiên ma ngoại đạo. ' +
-      'Giả sử có người học được cũng là bọn tinh mị dã hồ nương tựa cỏ cây.' +
+      'Giả sử có người học được cũng là bọn tinh mị dã hồ nương tựa cỏ cây. ' +
       'Ở đây ta không có một pháp cho các ông sanh phải là thiện tri thức, không biết gì cả, chỉ biết đi tri giải, chính ta cũng không hiểu thiền, cũng không Tổ, đều hoằng tiêu, đi tiểu, ăn cơm mặc áo, lại có việc gì?',
     speechType: '',
   },
