730a2f72ac606146e872fcce6163b0f4bf6bcc55
trann <trannhukhoinguyen@gmail.com>
2025-03-14 14:21:00 +0700

    update Unborn Mind

--- src/db/teachings/vi/UnbornMind.tsx
+++ src/db/teachings/vi/UnbornMind.tsx
@@ -4,11 +4,34 @@ const UnbornMind = {
   playlistId: 'PLJhUBezoFJ-3lFkC0KYPMN5f1v4e8hHWm',
   subTitle: 'Ngữ lục Thiền sư Bankei',
   author: '[Giáo sư] Yoshito Hakeda',
-  translator: 'Peter Haskel dịch Nhật -> Anh, [Ni sư] Thích Nữ Trí Hải Anh -> Việt',
+  translator: 'Peter Haskel (Nhật -> Anh), [Ni sư] Thích Nữ Trí Hải (Anh -> Việt)',
   image: '/images/teachings/unborn-mind.jpg',
   intro:
-    '',
+    'Ngay cả vào thời đại của ngài, pháp thiền của Bankei cũng được xem như một sự cố bất thường, không giống bất cứ giáo lý nào người ta đã từng biết đến.' +
+    'Dù vấn đề gì, đối với Bankei cũng chỉ có một lối giải quyết, đó là xử lý mọi sự trên một căn bản hoàn toàn mới mẻ: hãy buông xả, hãy phóng khoáng tự nhiên, hãy tin tưởng vào cái tâm thực sự nguyên ủy của ta. ' +
+    'Ngài gọi đây là giáo lý về Bất sinh, hay về cái Tâm Phật Bất Sinh.',
   paragraph1:
+    '☀️ Lời mở ☀️ / ' +
+    'Khi thiền sư Bankei Butchi Kosai – người sáng lập thiền tự Ryòmonji (Long môn) ở Aboshi thuộc tỉnh Banshù – mở kỳ kiết thất lớn tại Long môn vào mùa đông năm thứ ba Genroku (1690), có 1.683 tu sĩ ghi danh trong sổ danh bạ. ' +
+    'Những người đến nghe gồm không những đồ chúng thuộc tông Thiên thai và Tào động mà cả các tông phái Ritsu, Shingon (chân ngôn), Tendai, Tịnh độ, và Nhật liên tông, cả tăng lẫn tục cùng chen nhau trong giảng đường. ' +
+    'Người ta có cảm tưởng ngài đích thực là bậc Thiên nhân sư (Thầy của Người và Trời) của thời nay. ' +
+    'Khi ấy Sư bước lên pháp tòa và bảo đại chúng tăng tục:' +
+    'Hôm nay trong đại hội này chúng ta gồm cả một đám đông tăng tục, và có lẽ tôi đã kể cho quý vị nghe làm thế nào, lúc còn trẻ, tôi đã trực ngộ ra rằng cái tâm vốn dĩ chưa từng sinh ra. ' +
+    'Cái phần về tâm ấy tuy thế, chỉ là điều phụ thuộc. / ' +
+    'Quý vị tu sĩ, khi quý vị an trú trong Bất sinh, quý vị sẽ thấy không có gì người khác cần nói với quý vị, cũng không có gì quý vị cần nghe. ' +
+    'Vì Tâm Phật bất sinh đang chiếu sáng kỳ diệu, nên nó dễ dàng biến thành bất cứ gì nó gặp phải. ' +
+    'Bởi thế, trong khi tôi bảo mọi người tại gia ở đây chớ có biến mình thành đủ loại mà họ gặp, và đổi cái tâm Phật của họ thành những ý tưởng, thì quý vị tu sĩ cũng có thể nghe luôn!',
+  paragraph2:
+    '☀️ Lắng nghe ☀️ / ' +
+    'Sư bảo chúng:' +
+    'Trong tất cả quý vị tập trung tại đây hôm nay, không một ai là người chưa giác ngộ. Mỗi người ở đây đều là một vị Phật. ' +
+    'Bởi thế hãy lắng nghe cho kỹ. Cái mà tất cả quý vị thừa hưởng của cha mẹ lúc mới lọt lòng, chỉ là cái tâm Phật bất sinh. ' +
+    'Không có cái bẩm sinh nào khác ở nơi quý vị. / ' +
+    'Cái tâm Phật mà quý vị có từ lúc cha mẹ mới sinh ra thực sự là bất sinh và chiếu sáng một cách kỳ diệu, và hơn thế nữa, với cái Bất sinh này, mọi sự đều được thu xếp một cách hoàn toàn ổn thỏa.' +
+    'Bằng chứng về điều này là, trong lúc tất cả quý vị đang lắng nghe tôi nói, mà ngoài kia có tiếng quạ kêu, tiếng chim sẻ chíp chíp, tiếng gió xào xạc… mặc dù không cố tình phân biệt những âm thanh ấy, quý vị vẫn nhận ra và phân biệt được từng tiếng: tiếng chim quạ, tiếng chim sẻ, tiếng gió xao cành – quý vị nghe mà không lầm lẫn, đấy gọi là nghe với cái Bất sinh. ' +
+    'Với cái Bất sinh, tất cả mọi sự được thu xếp hoàn toàn ổn thỏa cũng y như vậy. Đấy là bằng chứng thực sự về cái Bất sinh. Hãy nhận ra một cách dứt khoát rằng cái không sinh và đang chiếu sáng kỳ diệu ấy đích thực là Tâm Phật. ' +
+    'An trú ngay trong Tâm Phật Bất Sinh ấy, thì từ đây cho đến mãi mãi quý vị là một đức Như Lai sống. ' +
+    'Vì khi đã nhận ra một cách rốt ráo thì từ nay trở đi quý vị sẽ luôn an trú trong Tâm Phật, nên trường phái của tôi mệnh danh là trường phái Phật tâm. ' +
     '',
   verse:
     '',
