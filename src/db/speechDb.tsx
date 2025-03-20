abcf909c948ba682c70cdceb73544a942c969ec9
Miguel <miguel@glodival.vn>
2025-03-20 22:50:57 +0700

    update speeches

--- src/db/speechDb.tsx
+++ src/db/speechDb.tsx
@@ -1,20 +1,22 @@
 import ShakyamuniBuddha from "../db/speeches/ShakyamuniBuddha.tsx";
+import LaiguoMiaoshu from "../db/speeches/LaiguoMiaoshu.tsx";
 import NanyangHuizhong from "../db/speeches/NanyangHuizhong.tsx";
+import XuanshaShibei from "../db/speeches/XuanshaShibei.tsx";
 import LinjiYixuan from "../db/speeches/LinjiYixuan.tsx";
 import FoyanQingyuan from "../db/speeches/FoyanQingyuan.tsx";
 import YangqiFanghui from "../db/speeches/YangqiFanghui.tsx";
-import XuanshaShibei from "../db/speeches/XuanshaShibei.tsx";
 import YunmenWenyan from "../db/speeches/YunmenWenyan.tsx";
 import DeshanXuanjian from "../db/speeches/DeshanXuanjian.tsx";
 import GuifengZongmi from "../db/speeches/GuifengZongmi.tsx";
 
 const speechDb = [
   ...ShakyamuniBuddha,
+  ...LaiguoMiaoshu,
+  ...XuanshaShibei,
   ...NanyangHuizhong,
   ...LinjiYixuan,
   ...FoyanQingyuan,
   ...YangqiFanghui,
-  ...XuanshaShibei,
   ...YunmenWenyan,
   ...DeshanXuanjian,
   ...GuifengZongmi,
--- src/db/speeches/XuanshaShibei.tsx
+++ src/db/speeches/XuanshaShibei.tsx
@@ -5,22 +5,90 @@ const XuanshaShibei = [
     authorImage: '/images/wide/cloudy-mountain-peak.jpg',
     authorLink: '/masters/Xuansha-Shibei',
     speechMain:
+      'Mặt trời trên thái hư vì tất cả mọi người mà thành lập. ' +
+      'Thái hư lúc nào cũng có đó mà tại sao mọi người căng mắt ra nhìn mà không thấy, căng tai mà không nghe. ' +
+      'Hai nơi mắt tai mà không rõ rệt thì đó là gã ngủ gục. ' +
+      'Nếu mà minh triệt thì vượt cả phàm Thánh, vượt cả ba giới. ' +
+      'Mộng ảo thân tâm không một vật như đầu mũi kim nhọn vi duyên, vi dối.' +
+      'Dẫu cho chư Phật có xuất lai, biến hiện vô hạn thần thông. ' +
+      'Ví như rất nhiều giáo cương, nhưng chưa từng chỉ ra một phân hào nào cả mà chỉ hỗ trợ cho người sơ học thành tín. ' +
+      'Có lãnh hội không? Chim nước ngụ trong rừng hiểu rõ đề cương, rất là đoan đích. ' +
+      'Tự thị ít người nghe không phải là chuyện nhỏ. ' +
+      'Thiên ma ngoại đạo là quân vô ơn, bạc nghĩa. Thiên nhân sáu đường là tự dối, tự gạt. ' +
+      'Như nay Sa-môn không ngộ chuyện ấy, trở thành gã lộng ảnh, nổi chìm trong biển sanh tử, biết đến bao giờ mới dừng nghỉ được. ' +
+      'Tự nhà mình may mà còn có môn phong rộng lớn, không thiệu long kế thừa được, lại hướng thân điền năm ấm làm chủ tể. Có nằm mộng thấy chăng? ' +
+      'Như biết bao ruộng đất khiến ai làm chủ tể. Đại địa chở không nổi, hư không bao phủ không hết, há là chuyện nhỏ ru? ' +
+      'Nếu muốn thấu triệt thì nay đây nơi mình minh triệt đi. Chẳng khiến trong hai lấy một pháp lớn như hạt bụi con. Không khiến hai lấy một pháp lớn như tóc tơ. ' +
+      'Lãnh hội không? // ' +
+      '\n' +
       'Ta vì ông như thế là đã thành khuất khúc với người rồi. Như nay đây tự khẳng đương nhân phần thượng. ' +
-      '  Chẳng kể mới học vào tùng lâm, đều có thể coi như cùng mọi người bước vào lâu rồi, cùng với các Phật trong quá khứ chẳng có chi thiếu kém. ' +
-      '  Như nước ở biển cả, thì tất cả rồng cá từ mới sanh cho tới già lão, đều nuốt phun thọ dụng bình đẳng một thứ thôi. ' +
-      '  Cho nên người mới phát tâm học đạo cùng với cổ Phật ngang vai nhau. Chỉ kẹt cái là ông từ vô thỉ tích kiếp động chư vọng tình, kết thành phiền não. ' +
-      '  Như người bệnh nặng, tâm cồn cào nóng bức điên đảo thấy bậy bạ, đều không phải là sự thật. ' +
-      '  Như nay đây nhìn thấy mọi cảnh giới đều cũng như thế. Chư căn đối với các vị, đều thành điên đảo. ' +
-      '  Người xưa lấy thuốc diệu vô cùng để đối trị. Cho đến cả bậc thập địa vẫn còn chưa tỉnh táo. ' +
-      '  Do đó mà biết không phải dễ dàng. Người xưa tham cứu còn hơn là mất cả cha mẹ. ' +
-      '  Còn như các anh em ngày nay thấy là chuyện thường thì nơi đâu có người hiểu giùm cho anh em đây? ' +
-      '  Khá tiếc là thời gian đã bị để luống qua, có hại chi việc tự cứu xét nghiêm mật, quan sát tìm kiếm kỹ lưỡng, cho đến chỗ không trước lực, tự ngừng nghỉ chư duyên, dầu cho chưa nứt mầm thì hột giống cũng đã có mặt rồi. ' +
-      '  Còn nếu phải đi từ nhà mất bao khí lực đánh trống mưu cầu cơm cháo, đem cái hời hợt đó hiển bày chuyện sanh tử, lừa gạt các anh em một đời thì có ích lợi gì đâu! Nên như thật mà tri thủ là tốt hơn. ' +
-      '  Không có chuyện chi nữa. Tạm biệt! // ' +
+      'Chẳng kể mới học vào tùng lâm, đều có thể coi như cùng mọi người bước vào lâu rồi, cùng với các Phật trong quá khứ chẳng có chi thiếu kém. ' +
+      'Như nước ở biển cả, thì tất cả rồng cá từ mới sanh cho tới già lão, đều nuốt phun thọ dụng bình đẳng một thứ thôi. ' +
+      'Cho nên người mới phát tâm học đạo cùng với cổ Phật ngang vai nhau. Chỉ kẹt cái là ông từ vô thỉ tích kiếp động chư vọng tình, kết thành phiền não. ' +
+      'Như người bệnh nặng, tâm cồn cào nóng bức điên đảo thấy bậy bạ, đều không phải là sự thật. ' +
+      'Như nay đây nhìn thấy mọi cảnh giới đều cũng như thế. Chư căn đối với các vị, đều thành điên đảo. ' +
+      'Người xưa lấy thuốc diệu vô cùng để đối trị. Cho đến cả bậc thập địa vẫn còn chưa tỉnh táo. ' +
+      'Do đó mà biết không phải dễ dàng. Người xưa tham cứu còn hơn là mất cả cha mẹ. ' +
+      'Còn như các anh em ngày nay thấy là chuyện thường thì nơi đâu có người hiểu giùm cho anh em đây? ' +
+      'Khá tiếc là thời gian đã bị để luống qua, có hại chi việc tự cứu xét nghiêm mật, quan sát tìm kiếm kỹ lưỡng, cho đến chỗ không trước lực, tự ngừng nghỉ chư duyên, dầu cho chưa nứt mầm thì hột giống cũng đã có mặt rồi. ' +
+      'Còn nếu phải đi từ nhà mất bao khí lực đánh trống mưu cầu cơm cháo, đem cái hời hợt đó hiển bày chuyện sanh tử, lừa gạt các anh em một đời thì có ích lợi gì đâu! Nên như thật mà tri thủ là tốt hơn. ' +
+      'Không có chuyện chi nữa. Tạm biệt! // ' +
       '\n' +
       'Phàm người học Bát nhã Bồ tát phải đủ đại căn cơ, đại trí tuệ mới được. ' +
       'Nếu căn cơ trì độn, phải chuyên cần khổ nhọc, nhẫn nại đêm ngày quên mệt, giống như người đi đưa đám ma mẹ, cần cù cấp bách sẽ có người phụ lực. ' +
-      'Ghi tạc trong lòng chuyên tâm thật cứu, ắt có ngày hội ngộ. // ',
+      'Ghi tạc trong lòng chuyên tâm thật cứu, ắt có ngày hội ngộ. // ' +
+      '\n' +
+      'Núi sâu vực thẳm ngàn năm muôn năm không thấy bóng người đến, lại có Phật pháp chăng ? Nếu nói có, gọi cái gì là Phật pháp. Nếu nói không, Phật pháp cũng có chỗ không đạt đến. // ' +
+      '\n' +
+      'Chân tông của cổ Phật thường tùy muôn vật mà hiện bày, ứng dụng rõ ràng, nơi nơi rành rành sáng suốt, ẩn hiện thản nhiên, chỗ cao chỗ thấp đều được soi đến. ' +
+      'Do đó, bậc Sa-môn thượng sĩ sáng mắt, trước phải khế hợp cội nguồn, phát minh bản tâm mới là rốt ráo. ' +
+      'Muôn sự muôn pháp một thể đồng nguồn, thênh thang không ngần mé, còn gì phải chấp pháp hữu vi ? Những việc từ vô số kiếp đến giờ đều ở trước mắt.' +
+      'Người thời nay cách xa Phật Tổ, thậm chí trái mất bản thể thường hằng, trong tâm mê hoặc chấp vào muôn pháp, xa hẳn chân tông, không gặp được thầy lành bạn tốt nên tự có kiến giải riêng tư, dù có nhận thức cũng chỉ là kiến đoán mò mà thôi. ' +
+      'Đến như xét tột cùng bản thể cũng không phân biệt được tà chánh, huống là chính mình từ xưa đến giờ chưa từng giáp mặt bản tâm. // ' +
+      '\n' +
+      'Nếu là bậc cổ đức tiên hiền, thì phải tự biế thời tiết khắc chế lấy mình, tích lũy công đức, giam mình vào trong am tranh thất đá mà tu hành. ' +
+      'Cổ đức nói : “Nếu muốn cầu cảnh giới thánh nhân, vẫn còn kẹt trong pháp trần, tri kiến chưa dứt trừ thì cũng là lầm lẫn mà thôi”. ' +
+      'Không nên cho rằng trì trai giữ giới, ngồi mãi chẳng nằm, trụ ý quán không, lắng thần nhập định mà đạt được yếu chỉ thiền, hoàn toàn không dính dáng gì cá Ngoại đạo ở Tây Thiên nhập định được tám muôn kiếp, lắng thần lặng lẽ, nhắm mắt che tròng, thân không còn cảm giác, nhưng kiếp số đã mãn cũng không tránh khỏi luân hồi. ' +
+      'Bởi vì đạo nhãn không sáng, nên không phá nổi cội gốc sanh tử. // ' +
+      '\n' +
+      'Người xuất gia thì không phải vậy, không giống đủ đại tri kiến mới có thể triệt ngộ như chư Phật. ' +
+      'Tâm rỗng lặng mà thường chiếu soi, quên hết cảm giác, rỗng sáng bao hàm muôn vật. Hiện giờ chỗ nào chẳng phải ông, chỗ nào chẳng rõ ràng, chỗ nào chẳng hiện bày. ' +
+      'Tại sao không lãnh hội như thể ? Nếu ông không có kiến giải ấy thì thế nào cũng lầm lẫn mà thôi. Tất cả thành hư vọng, thì làm sao được chỗ đắc lực bình sanh. // ' +
+      '\n' +
+      'Nếu ông chưa phát minh bản tâm, phải khẩn thiết quên ăn bỏ ngủ, như cứu lửa cháy đầu, như tan thân mất mạng, tự cứu lấy mình, buông bỏ muôn duyên, lặng dừng tâm thức, như thế mới có chút phần thân cận Thiền tông. ' +
+      'Nếu không được như thế, sớm chiều bị thức tình lôi đi, làm sao được tự do tự tại? // ' +
+      '\n' +
+      'Bây giờ, có những vật vô tình thuyết pháp vô cùng chân thật mà ít ai nghe được. Nếu nghe được vô tình thuyết pháp thì mới có thể thương lượng. ' +
+      'Hãy nói vô tình thuyết pháp làm sao thương lượng? Hãy nói xem ? ' +
+      'Nhưng không được nói: “Không có ngôn thuyết, không thấy không nghe. ' +
+      'Không có người hỏi mà tự nói và khen ngợi đạo pháp của ta”. ' +
+      'Ông chẳng thấy Thiện Tài Đồng Tử tham vấn 53 thiện tri thức, rốt sau gặp ngài Di-lặc trong khoảng khảy móng tay mà được vào cửa. ' +
+      'Vừa vào cửa, cửa liền đóng lại, Thiện Tài Đồng Tử ở trong lầu các thấy trăm ngàn chư Phật quá khứ xả thân, thọ thân. ' +
+      'Tham vấn xem 128 thiện tri thức đều ở trong cảnh giới lầu các đồng thời hiện ra chứng minh cho ông, tâm suy nghĩ của Thiện Tải tức thời dứt bặt.  // ' +
+      '\n' +
+      'Nếu thiền tăng có đủ kiến giải chân thật này phát minh bản tâm mới có thể thương lượng. ' +
+      'Lúc bấy giờ ở trong lục đạo tứ sanh như ở cõi Tịnh Độ chư Phật, đâu còn lo sợ sanh tử nữa. ' +
+      'Vả lại, ai biết được tất cả pháp đều không thực thể! ' +
+      'Trên hội Linh Sơn chính ngài Ca-diếp nghe đức Thế Tôn phó pháp, việc đó chẳng khác nào dùng ngôn ngữ để bàn luận Phật tánh thôi. // ' +
+      '\n' +
+      'Cổ đức nói: “Thiện ác đều không nghĩ”. Như dùng ngón tay chỉ mặt trăng, cho đến cảnh giới Tam thừa, Bồ-tát được Niết-bàn, giải thoát đều như hoa đóm trong hư không, như là sừng thỏ. ' +
+      'Đâu chẳng nghe nói: “Xem thế gian này như việc trong mộng”. ' +
+      'Không nên nương tựa pháp hữu vi, nương tựa không ích lợi gì cả. ' +
+      'Bởi vì trái với chân tánh, bỏ mất bản tâm. ' +
+      'Xa lìa phàm tục, để tâm nơi thánh đạo, thấy biết như thế còn không thể vượt ra khỏi giới hạn phàm thánh vì không thoát được thân ngũ ấm. ' +
+      'Kinh nói: “Các hành vô thường là pháp sanh diệt”. ' +
+      'Ông chỉ muốn hướng tâm đến trước, làm sao hiểu được đạo lý? Chỉ có thấu suốt được đạo lý này mới có thể biết được.  // ' +
+      '\n' +
+      'Nếu chưa được rốt ráo, nên biết chỉ là thế giới rỗng không, khó mà tin được. ' +
+      'Người đủ đại căn khí mới có thể thấu rõ việc hiện đời. ' +
+      'Nếu chưa thấu triệt, dù trải qua muôn kiếp cũng không biết được. // ' +
+      '\n' +
+      'Cổ đức nói: // ' +
+      'Đời nay nếu ai liễu ngộ được ' +
+      'Kiếp sau nào phải thọ tai ương. // ' +
+      'Trực hướng kim sanh tu liễu khước ' +
+      'Thùy năng lụy kiếp thọ dư ương. // ' +
+      'Trân trọng!',
     speechType: '',
   },
 ];
