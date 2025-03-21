83fe06d0dc0fa894aff5f0ef3c86521f99056742
Patriarchal Zen <trannhukhoinguyen@gmail.com>
2025-03-21 23:18:12 +0700

    Create HuangboXiyun.tsx

---
+++ src/db/speeches/HuangboXiyun.tsx
@@ -0,0 +1,13 @@
+const HuangboXiyun = [
+{
+    id: 3745,
+    authorName: 'Hoàng Bá Hy Vận',
+    authorImage: '/images/wide/cloudy-mountain-peak.jpg',
+    authorLink: '/masters/Huangbo-Xiyun',
+    speechMain:
+      '',
+    speechType: '',
+  },
+];
+
+export default HuangboXiyun;
