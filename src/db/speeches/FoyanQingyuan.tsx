0f6aa63690a40922e3c92a4cd1b5a5923e3392a1
Miguel <miguel@glodival.vn>
2025-03-23 20:15:02 +0700

    update speech

--- src/db/speeches/FoyanQingyuan.tsx
+++ src/db/speeches/FoyanQingyuan.tsx
@@ -5,12 +5,32 @@ const FoyanQingyuan = [
     authorImage: '/images/wide/cloudy-mountain-peak.jpg',
     authorLink: '/masters/Foyan-Qingyuan',
     speechMain:
+      'Trong thân có sanh già bệnh chết, trên niệm có sanh trụ dị diệt, cõi nước có thành trụ hoại không. Mười hai loại việc này rất có thể kỳ đặc. ' +
+      'Phàm phu chẳng biết, vì họ nổi trôi. Đức Như Lai ra đời chỉ ra Niết bàn diệu tâm, thường lạc ngã tịnh. Giống như một viên đan sa điểm sắt thành vàng. ' +
+      'Một lời tột lý chuyển phàm thành thánh. Mười hai loại này chỉ là một pháp. Hiện định như nay là người nghe pháp rõ ràng vậy. Lại nói được đến chăng? ' +
+      'Bèn nói: “Đan quế trong trăng riêng cấm lạnh, mai lạnh trong tuyết riêng phóng hương. Xuống tòa. // ' +
+      '\n' +
+      'Như tăng hỏi Đức Sơn: “Thế nào là việc kỳ đặc?”. Đức Sơn nói: “Tông ta không ngữ cú, không một pháp cho người”. // ' +
+      '\n' +
       'Gươm Thái A tiếp ngang, cắt đứt muôn cơ ở trong nắm tay đang nắm. ' +
       'Gương Bảo Giám đang ở trên đài, hiện bày các hình bóng vô tâm. ' +
       'Có duyên liền ứng, nên hỏi đáp để tung hoành, chẳng khiến mà hành, bèn nói hình bóng mà hiếm so lường. ' +
       'Người có ảnh hưởng, ấy là đủ sao? Cho nên, Phật giao cho Phật, Tổ truyền cho Tổ. ' +
       'Lại không chút gì khác, há lại có khác biệt ở Đông ở Tây sao? Chẳng lập thứ lớp, riêng truyền việc ấy. ' +
       'Nếu chẳng phải là linh căn đốn ngộ, đại dụng hiện tiền, chưa thoát khỏi biển sâu của nghiệp mê lầm, vọng trần tự cách. // ' +
+      '\n' +
+      'Sơn tăng vừa ở trong tẩm đường (nhà bệnh), trên pháp đường (nhà thuyết pháp) không có Sơn tăng, trên tẩm đường có Sơn tăng. ' +
+      'Xuống tới pháp đường, trên pháp đường có Sơn tăng, trên tẩm đường không có Sơn tăng. ' +
+      'Có thì ngoài tâm có pháp, không thì ngoài tâm, pháp chẳng tròn. ' +
+      'Chư thượng tọa, ở dưới chỗ để y bát nghe đánh bảng liền lên pháp đường, trên pháp đường được thêm thượng tọa, dưới chỗ để y bát thiếu thượng tọa. ' +
+      'Thêm thành tăng, giảm thành diệt. ' +
+      'Diệt nên rơi vào đoạn, tăng thì rơi vào thường. ' +
+      'Người hành cước làm sao lìa được có và không, lìa được thường và đoạn? Sanh tử nghi tình rất khó thấu thoát. ' +
+      'Đây là tâm thanh tịnh của Như Lai, nên nghi phải quyết trạch, chẳng nên ngồi không. ' +
+      'Ngày tháng đổi dời như tên bắn, đời phù du như thế, người sống có bao lâu? ' +
+      'Kia đây xuất gia, làm khách nghịch trong ba cõi, nhà tranh cửa trúc, ai làm chủ đó? ' +
+      'Lãnh đạm ở chung, vắng vẻ cùng ở. ' +
+      'Vì sao? Mới gì? Lo gì? Nghĩ gì? Xuống tòa. // ' +
       '\n' +
       'Lại thấy chăng? Nếu thấy, thấy cái gì? Nếu thấy cây phất tử chính là phàm phu. ' +
       'Nếu nói chẳng thấy, thì đây là cây phất tử, sao nói là chẳng thấy? ' +
@@ -32,6 +52,50 @@ const FoyanQingyuan = [
       'Các ông quyết phải thật sự chứng nghiệm lấy, ngay đó phải cắt đứt đầu lưỡi, buông hết thân tâm, tự nhiên sông núi vui vẻ. ' +
       'Mắt nếu chẳng buồn ngủ thì các mộng tự trừ, tâm bằng chẳng khác thì gọi là vật gì? ' +
       'Sống vui thích! Sống vui thích! Về nhà uống trà đi! Xuống tòa. // ' +
+      '\n' +
+      'Nếu có một người phát về nguồn chơn, 10 phương hư không hết thảy đều tiêu mất hết. ' +
+      'Chư Thánh từ trước há chẳng phát về nguồn chân sao? Vậy làm sao 10 phương thế giới hư không đến nay vẫn còn? ' +
+      'Lại nói: Bọt tan không vốn không, huống lại có ba cõi! Thân huyễn đã diệt rồi nhưng hư không diệt thì không. ' +
+      'Chúng sanh trong 3 cõi từ đây cũng không còn khốn khổ nữa. 9 loài, 4 loài làm sao được không? ' +
+      'Lại nói: “Thanh tịnh vốn sẵn vậy, vì sao bỗng sanh ra sơn hà đại địa?”. ' +
+      'Đã sanh ra sơn hà đại địa thì làm sao trở lại thanh tịnh sẵn vậy? ' +
+      'Làm sao bỏ cái chấp thấy sơn hà đại địa? Đại chúng! ' +
+      'Làm sao là phải? Im lặng giây lâu, nói: “Nước từ bờ trúc theo dòng lạnh, gió ở trong hoa thoảng hương đưa. ' +
+      'Hảo đại ca, Hảo đại ca! Về nhà." // ' +
+      '\n' +
+      'Trên đường Ngũ Đài Sơn khách đi qua rất ít. Trước nhà Phá Táo (Đọa) cảm ơn không nơi. Tuyết chôn cây bách (trước) sân, băng đông cứng nơi khe suối. ' +
+      'Lò lửa tuy ở phương Nam, chẳng vào trong hũ dưa muối nhà người. Coi đây, coi đây, ngày 30 tháng Chạp chính là Mạnh Xuân còn Đông. ' +
+      'Bọn các ông, mỗi mỗi phải cố gắng hướng tới trước. Rất kỵ tự sanh lui sụt. Xuống tòa. // ' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
       '\n' +
       '',
     speechType: '',
